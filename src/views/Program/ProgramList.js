01c017787c89abf8d11d12f083542eec70df4a9e
palash-nikam-altius <palash.n@altius.cc>
2020-03-28 20:07:13 +0530

    program master list commit by palash

--- src/views/Program/ProgramList.js
+++ src/views/Program/ProgramList.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import { NavLink } from 'react-router-dom';
-import { Card, CardHeader, CardBody, Button } from 'reactstrap';
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import { Card, CardHeader, CardBody, FormGroup, Input, InputGroup, InputGroupAddon, Label, Button, Col } from 'reactstrap';
 import 'react-bootstrap-table/dist//react-bootstrap-table-all.min.css';
 import getLabelText from '../../CommonComponent/getLabelText';
 import programDate from './ProgramData';
 import ProgramService from "../../api/ProgramService";
 import AuthenticationService from '../Common/AuthenticationService.js';
 import i18n from '../../i18n';
+import CountryService from '../../api/CountryService.js';
+import BootstrapTable from 'react-bootstrap-table-next';
+import filterFactory, { textFilter, selectFilter, multiSelectFilter } from 'react-bootstrap-table2-filter';
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
+import paginationFactory from 'react-bootstrap-table2-paginator'
 
 const entityname = i18n.t('static.program.programMaster');
 export default class ProgramList extends Component {
@@ -15,35 +19,32 @@ export default class ProgramList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      table: [],
+      programlist: [],
       lang: 'en',
-      message:''
+      message: '',
+      selProgram: [],
+      countryList: [],
+      lang: localStorage.getItem('lang')
     }
-    // this.table = programDate.rows;
-    this.options = {
-      sortIndicator: true,
-      hideSizePerPage: true,
-      paginationSize: 3,
-      hidePageListOnlyOnePage: true,
-      clearSearch: true,
-      alwaysShowAllBtns: false,
-      withFirstAndLast: false,
-      onRowClick: function (row) {
-        this.editProgram(row);
-      }.bind(this)
-    }
-
-    this.showProgramLabel = this.showProgramLabel.bind(this);
-    this.showRealmLabel = this.showRealmLabel.bind(this);
-    this.showCountryLabel = this.showCountryLabel.bind(this);
-    this.showOrganisationLabel = this.showOrganisationLabel.bind(this);
     this.editProgram = this.editProgram.bind(this);
     this.addNewProgram = this.addNewProgram.bind(this);
     this.buttonFormatter = this.buttonFormatter.bind(this);
     this.addProductMapping = this.addProductMapping.bind(this);
+    this.filterData = this.filterData.bind(this);
+  }
 
-
-
+  filterData() {
+    let countryId = document.getElementById("countryId").value;
+    if (countryId != 0) {
+      const selProgram = this.state.programList.filter(c => c.realmCountry.country.countryId == countryId)
+      this.setState({
+        selProgram: selProgram
+      });
+    } else {
+      this.setState({
+        selProgram: this.state.programList
+      });
+    }
   }
 
   editProgram(program) {
@@ -52,60 +53,93 @@ export default class ProgramList extends Component {
       // state: { program }
     });
   }
-  // just an example
-  // nameFormat(cell, row) {
-  //   const id = `/budgets/${row.id}`
-  //   return (
-  //     <NavLink strict to={id}> {cell} </NavLink>
-  //   );
-  // };
-
   componentDidMount() {
     AuthenticationService.setupAxiosInterceptors();
     ProgramService.getProgramList().then(response => {
-      this.setState({
-        table: response.data
-      })
+      if (response.status == 200) {
+        this.setState({
+          programList: response.data,
+          selProgram: response.data
+        })
+      } else {
+        this.setState({ message: response.data.messageCode })
+      }
     })
       .catch(
         error => {
-          switch (error.message) {
-            case "Network Error":
-              this.setState({
-                message: error.message
-              })
-              break
-            default:
-              this.setState({
-                message: error.message
-              })
-              break
+          if (error.message === "Network Error") {
+            this.setState({ message: error.message });
+          } else {
+            switch (error.response.status) {
+              case 500:
+              case 401:
+              case 404:
+              case 406:
+              case 412:
+                this.setState({ message: error.response.data.messageCode });
+                break;
+              default:
+                this.setState({ message: 'static.unkownError' });
+                break;
+            }
+          }
+        }
+      );
+    CountryService.getCountryListActive().then(response => {
+      if (response.status == 200) {
+        console.log("response--->", response.data);
+        this.setState({
+          countryList: response.data,
+        })
+      } else {
+        this.setState({ message: response.data.messageCode })
+      }
+    })
+      .catch(
+        error => {
+          if (error.message === "Network Error") {
+            this.setState({ message: error.message });
+          } else {
+            switch (error.response.status) {
+              case 500:
+              case 401:
+              case 404:
+              case 406:
+              case 412:
+                this.setState({ message: error.response.data.messageCode });
+                break;
+              default:
+                this.setState({ message: 'static.unkownError' });
+                break;
+            }
           }
         }
       );
 
-  }
 
-  showProgramLabel(cell, row) {
-    // console.log("========", cell);
-    return getLabelText(cell, this.state.lang);
 
   }
-  showRealmLabel(cell, row) {
-    // console.log("========>",cell);
-    return getLabelText(cell.realm.label, this.state.lang);
 
-  }
-  showCountryLabel(cell, row) {
-    // console.log("========>",cell);
-    return getLabelText(cell.country.label, this.state.lang);
+  // showProgramLabel(cell, row) {
+  //   // console.log("========", cell);
+  //   return getLabelText(cell, this.state.lang);
 
-  }
-  showOrganisationLabel(cell, row) {
-    // console.log("========>",cell);
-    return getLabelText(cell.label, this.state.lang);
+  // }
+  // showRealmLabel(cell, row) {
+  //   // console.log("========>",cell);
+  //   return getLabelText(cell.realm.label, this.state.lang);
 
-  }
+  // }
+  // showCountryLabel(cell, row) {
+  //   // console.log("========>",cell);
+  //   return getLabelText(cell.country.label, this.state.lang);
+
+  // }
+  // showOrganisationLabel(cell, row) {
+  //   // console.log("========>",cell);
+  //   return getLabelText(cell.label, this.state.lang);
+
+  // }
   addNewProgram() {
     this.props.history.push({
       pathname: "/program/addProgram"
@@ -113,7 +147,7 @@ export default class ProgramList extends Component {
   }
   buttonFormatter(cell, row) {
     // console.log("-----------", cell);
-    return <Button type="button" size="sm" color="success" onClick={(event) => this.addProductMapping(event, cell)} className="float-right mr-1" ><i className="fa fa-check"></i> Add</Button>;
+    return <Button type="button" size="sm" color="success" onClick={(event) => this.addProductMapping(event, cell)} ><i className="fa fa-check"></i> Add</Button>;
   }
   addProductMapping(event, cell) {
     // console.log(cell);
@@ -122,103 +156,125 @@ export default class ProgramList extends Component {
     ProgramService.getProgramProductListByProgramId(cell)
       .then(response => {
 
-        let myReasponse = response.data.data;
-        console.log("myResponce=========", response.data.data);
-        this.props.history.push({
-          pathname: "/programProduct/addProgramProduct",
-          state: {
-            programProduct: myReasponse
-          }
+        if (response.status == 200) {
+          let myReasponse = response.data;
+          console.log("myResponce=========", response.data);
+          this.props.history.push({
+            pathname: "/programProduct/addProgramProduct",
+            state: {
+              programProduct: myReasponse
+            }
 
-        })
+          })
+        } else {
+          this.setState({
+            message: response.data.messageCode
+          })
+        }
       }).catch(
         error => {
-          switch (error.message) {
-            case "Network Error":
-              this.setState({
-                message: error.response
-              })
-              break
-            default:
-              this.setState({
-                message: error.response
-              })
-              break
+          if (error.message === "Network Error") {
+            this.setState({ message: error.message });
+          } else {
+            switch (error.response.status) {
+              case 500:
+              case 401:
+              case 404:
+              case 406:
+              case 412:
+                this.setState({ message: error.response.data.messageCode });
+                break;
+              default:
+                this.setState({ message: 'static.unkownError' });
+                console.log("Error code unkown");
+                break;
+            }
           }
         }
       );
-
-
-
-    // this.props.history.push({
-    //   pathname: "/programProduct/addProgramProduct",
-    //   state: {
-
-
-
-
-
-
-
-    //     'programProduct':
-
-    //     {
-    //       'programId': 1,
-    //       'label': {
-    //         'label_en': "Kenya Malaria"
-    //       },
-    //       'prodcuts':
-    //         [
-    //           {
-    //             'productId': 2,
-    //             'label': {
-    //               'label_en': "Abacavir"
-    //             },
-    //             'minMonth': 1,
-    //             'maxMonth': 3
-    //           },
-    //           {
-    //             'productId': 4,
-    //             'label': {
-    //               'label_en': "Condoms"
-    //             },
-    //             'minMonth': 1,
-    //             'maxMonth': 3
-    //           }
-    //         ]
-    //     }
-
-
-
-
-
-    // programId: 1,
-    // label: 'program 1',
-    // rows: [
-    //   {
-    //     programId: '1',
-    //     programName: 'Program 1',
-    //     productId: '1',
-    //     productName: 'product 1',
-    //     minMonth: '2',
-    //     maxMonth: '2'
-    //   },
-    //   {
-    //     programId: '1',
-    //     programName: 'Program 1',
-    //     productId: '2',
-    //     productName: 'product 2',
-    //     minMonth: '3',
-    //     maxMonth: '4'
-    //   }
-    // ]
-    // }
-
-
-
-
   }
   render() {
+
+    const { SearchBar, ClearSearchButton } = Search;
+    const customTotal = (from, to, size) => (
+      <span className="react-bootstrap-table-pagination-total">
+        {i18n.t('static.common.result', { from, to, size })}
+      </span>
+    );
+    const { countryList } = this.state;
+    let countries = countryList.length > 0
+      && countryList.map((item, i) => {
+        return (
+          <option key={i} value={item.countryId}>
+            {getLabelText(item.label, this.state.lang)}
+          </option>
+        )
+      }, this);
+
+    const columns = [
+      {
+        dataField: 'label.label_en',
+        text: i18n.t('static.program.program'),
+        sort: true,
+        align: 'center',
+        headerAlign: 'center'
+      },
+      {
+        dataField: 'realmCountry.realm.label.label_en',
+        text: i18n.t('static.program.realm'),
+        sort: true,
+        align: 'center',
+        headerAlign: 'center'
+      },
+      {
+        dataField: 'realmCountry.country.label.label_en',
+        text: i18n.t('static.program.realmcountry'),
+        sort: true,
+        align: 'center',
+        headerAlign: 'center'
+      },
+      {
+        dataField: 'organisation.label.label_en',
+        text: i18n.t('static.program.organisation'),
+        sort: true,
+        align: 'center',
+        headerAlign: 'center'
+      },
+      {
+        dataField: 'programId',
+        text: 'Map Product To Program',
+        sort: true,
+        align: 'center',
+        headerAlign: 'center',
+        formatter: this.buttonFormatter
+      }
+    ];
+    const options = {
+      hidePageListOnlyOnePage: true,
+      firstPageText: i18n.t('static.common.first'),
+      prePageText: i18n.t('static.common.back'),
+      nextPageText: i18n.t('static.common.next'),
+      lastPageText: i18n.t('static.common.last'),
+      nextPageTitle: i18n.t('static.common.firstPage'),
+      prePageTitle: i18n.t('static.common.prevPage'),
+      firstPageTitle: i18n.t('static.common.nextPage'),
+      lastPageTitle: i18n.t('static.common.lastPage'),
+      showTotal: true,
+      paginationTotalRenderer: customTotal,
+      disablePageTitle: true,
+      sizePerPageList: [{
+        text: '10', value: 10
+      }, {
+        text: '30', value: 30
+      }
+        ,
+      {
+        text: '50', value: 50
+      },
+      {
+        text: 'All', value: this.state.selProgram.length
+      }]
+    }
     return (
       <div className="animated">
         <h5>{i18n.t(this.props.match.params.message, { entityname })}</h5>
@@ -233,7 +289,28 @@ export default class ProgramList extends Component {
             </div>
           </CardHeader>
           <CardBody>
-            <BootstrapTable data={this.state.table} version="4" hover pagination search options={this.options}>
+            <Col md="3">
+              <FormGroup>
+                <Label htmlFor="appendedInputButton">{i18n.t('static.region.country')}</Label>
+                <div className="controls">
+                  <InputGroup>
+                    <Input
+                      type="select"
+                      name="countryId"
+                      id="countryId"
+                      bsSize="lg"
+                    >
+                      <option value="0">{i18n.t('static.common.select')}</option>
+                      {countries}
+                    </Input>
+                    <InputGroupAddon addonType="append">
+                      <Button color="secondary" onClick={this.filterData}>{i18n.t('static.common.go')}</Button>
+                    </InputGroupAddon>
+                  </InputGroup>
+                </div>
+              </FormGroup>
+            </Col>
+            {/* <BootstrapTable data={this.state.table} version="4" hover pagination search options={this.options}>
               <TableHeaderColumn isKey dataField="programId" hidden >Program Id</TableHeaderColumn>
               <TableHeaderColumn filterFormatted dataField="label" dataSort dataFormat={this.showProgramLabel} >{i18n.t('static.program.program')}</TableHeaderColumn>
               <TableHeaderColumn filterFormatted dataField="realmCountry" dataSort dataFormat={this.showRealmLabel} >{i18n.t('static.program.realm')}</TableHeaderColumn>
@@ -245,7 +322,34 @@ export default class ProgramList extends Component {
               <TableHeaderColumn dataField="draftToSubmittedLeadTime" dataSort>{i18n.t('static.program.drafttosubmitleadtime')}</TableHeaderColumn>
               <TableHeaderColumn dataField="submittedToApprovedLeadTime" dataSort>{i18n.t('static.program.submittoapproveleadtime')}</TableHeaderColumn>
               <TableHeaderColumn dataField="programId" dataFormat={this.buttonFormatter}>Map Product To Program</TableHeaderColumn>
-            </BootstrapTable>
+            </BootstrapTable> */}
+            <ToolkitProvider
+              keyField="programId"
+              data={this.state.selProgram}
+              columns={columns}
+              search={{ searchFormatted: true }}
+              hover
+              filter={filterFactory()}
+            >
+              {
+                props => (
+                  <div>
+                    <hr />
+                    <SearchBar {...props.searchProps} />
+                    <ClearSearchButton {...props.searchProps} />
+                    <BootstrapTable noDataIndication={i18n.t('static.common.noData')} tabIndexCell
+                      pagination={paginationFactory(options)}
+                      rowEvents={{
+                        onClick: (e, row, rowIndex) => {
+                          this.editProgram(row);
+                        }
+                      }}
+                      {...props.baseProps}
+                    />
+                  </div>
+                )
+              }
+            </ToolkitProvider>
           </CardBody>
         </Card>
       </div>
