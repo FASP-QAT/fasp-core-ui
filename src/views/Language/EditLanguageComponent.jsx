7ba3b9a025ea9a7e56276759bf2a92d71cea09cf
shubham deulkar <shubham.d@altius.cc>
2020-04-28 08:27:27 +0530

    comitt by shubham

--- src/views/Language/EditLanguageComponent.jsx
+++ src/views/Language/EditLanguageComponent.jsx
@@ -130,8 +130,7 @@ export default class EditLanguageComponent extends Component {
         return (
             <div className="animated fadeIn">
                 <AuthenticationServiceComponent history={this.props.history} message={this.changeMessage} />
-                <h6>{i18n.t(this.state.message)}</h6>
-                <h6>{i18n.t(this.props.match.params.message)}</h6>
+                <h5>{i18n.t(this.state.message, { entityname })}</h5>
                 <Row>
                     <Col sm={12} md={6} style={{ flexBasis: 'auto' }}>
                         <Card>
