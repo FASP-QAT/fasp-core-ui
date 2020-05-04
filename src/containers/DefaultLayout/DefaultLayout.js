ba6dd5e553acc6964d88856a24ba13cf93672d36
palash-nikam-altius <palash.n@altius.cc>
2020-05-04 10:04:28 +0530

    commit by palash

--- src/containers/DefaultLayout/DefaultLayout.js
+++ src/containers/DefaultLayout/DefaultLayout.js
@@ -356,11 +356,11 @@ class DefaultLayout extends Component {
                       //     }
                       //   ]
                       // },
-                      // {
-                      //   name: 'Program Onboarding',
-                      //   url: '/program/programOnboarding',
-                      //   icon: 'icon-graph'
-                      // },
+                      ,{
+                        name: 'Program Onboarding',
+                        url: '/program/programOnboarding',
+                        icon: 'icon-graph'
+                      },
                     ]
 
                 }} {...this.props} />
