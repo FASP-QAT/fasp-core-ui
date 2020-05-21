96eba3e388727387368cd40652c29d6e4ebc41f5
juned rajjak sheikh <junaid.s@altius.cc>
2020-05-21 12:45:25 +0530

    syncpage message align

--- src/views/SyncMasterData/SyncMasterData.js
+++ src/views/SyncMasterData/SyncMasterData.js
@@ -59,7 +59,7 @@ export default class SyncMasterData extends Component {
         return (
             <div className="animated fadeIn">
                 <h6 className="mt-success">{i18n.t(this.props.match.params.message)}</h6>
-                <h5>{i18n.t(this.state.message)}</h5>
+                <h5 className="pl-md-5">{i18n.t(this.state.message)}</h5>
                 <div className="col-md-12"> 
                     <Col xs="12" sm="12">
                         <Card>
