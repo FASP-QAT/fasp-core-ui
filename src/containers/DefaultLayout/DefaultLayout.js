d23783ba70b6e1916cfc5d6b9c22f733738c4234
Ekta Nalawade <ekta.n@altius.cc>
2020-07-03 18:16:10 +0530

    stock status across planningUnit

--- src/containers/DefaultLayout/DefaultLayout.js
+++ src/containers/DefaultLayout/DefaultLayout.js
@@ -478,6 +478,13 @@ class DefaultLayout extends Component {
                             url: '/report/stockStatus',
                             icon: 'fa fa-line-chart',
                             attributes: { hidden: (this.state.businessFunctions.includes('ROLE_BF_STOCK_STATUS_REPORT') ? false : true) }
+                          }, ,
+                          {
+                            name: i18n.t('static.dashboard.stockstatusacrossplanningunit'),
+                            url: '/report/stockStatusAcrossPlanningUnits',
+                            icon: 'fa fa-exchange',
+                            attributes: { hidden: (this.state.businessFunctions.includes('ROLE_BF_STOCK_STATUS_REPORT') ? false : true) }
+                        
                           },
                           {
                             name: i18n.t('static.report.warehouseCapacity'),
@@ -693,12 +700,22 @@ class DefaultLayout extends Component {
                             url: '/report/aggregateShipmentByProduct',
                             icon: 'fa fa-wpforms',
                             attributes: { hidden: (this.state.businessFunctions.includes('ROLE_BF_PROCUREMENT_AGENT_REPORT') ? false : true) }
-                          }
-                          /* {
+                          },
+                           {
                              name: i18n.t('static.dashboard.stockstatus'),
                              url: '/report/stockStatus',
-                             icon: 'fa fa-exchange'
-                           }*/
+                             icon: 'fa fa-exchange',
+                             attributes: { hidden: (this.state.businessFunctions.includes('ROLE_BF_STOCK_STATUS_REPORT') ? false : true) }
+                         
+                           }
+                           ,
+                           {
+                             name: i18n.t('static.dashboard.stockstatusacrossplanningunit'),
+                             url: '/report/stockStatusAcrossPlanningUnits',
+                             icon: 'fa fa-exchange',
+                             attributes: { hidden: (this.state.businessFunctions.includes('ROLE_BF_STOCK_STATUS_REPORT') ? false : true) }
+                         
+                           }
                         ]
                       }
                     ]
