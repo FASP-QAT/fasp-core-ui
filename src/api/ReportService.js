588d2fec318f461a60314b02d65798e363183fa4
Ekta Nalawade <ekta.n@altius.cc>
2020-07-16 14:52:15 +0530

    supply plan review and report

--- src/api/ReportService.js
+++ src/api/ReportService.js
@@ -3,7 +3,7 @@ import { API_URL } from '../Constants.js';
 
 class ReportService {
     getForecastMatricsOverTime(json) {
-        return axios.post(`${API_URL}/api/report/forecastError`, json, {
+        return axios.post(`${API_URL}/api/report/forecastMetricsMonthly`, json, {
         });
     }
     getGlobalConsumptiondata(json) {
@@ -11,7 +11,7 @@ class ReportService {
         );
     }
     getForecastError(json) {
-        return axios.post(`${API_URL}/api/report/forecastMetrics`, json, {}
+        return axios.post(`${API_URL}/api/report/forecastMetricsComparision`, json, {}
         );
     }
 
