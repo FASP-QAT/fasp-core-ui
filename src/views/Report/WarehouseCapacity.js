36ebcece2952dc3482d2b507b9f27a2f2b6a6592
Anisha Selote <Anisha.s@altius.cc>
2020-07-21 16:56:56 +0530

    commited by anisha

--- src/views/Report/WarehouseCapacity.js
+++ src/views/Report/WarehouseCapacity.js
@@ -1075,7 +1075,7 @@ class warehouseCapacity extends Component {
                             }
                         </Offline>
                     </div>
-                    <CardBody className="pb-lg-2 pt-lg-0 ">
+                    <CardBody className="pb-lg-4 pt-lg-0 ">
                         <div className="" >
                             <div>
                                 <Form >
