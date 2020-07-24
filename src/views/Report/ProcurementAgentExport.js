f74bebad6cf4a03d72818616b0ac68ba593d143e
shubham deulkar <shubham.d@altius.cc>
2020-07-24 12:47:21 +0530

    commit by shubham

--- src/views/Report/ProcurementAgentExport.js
+++ src/views/Report/ProcurementAgentExport.js
@@ -1796,7 +1796,7 @@ class ProcurementAgentExport extends Component {
         csvRow.push(i18n.t('static.report.version').replaceAll(' ', '%20') + '  ,  ' + (document.getElementById("versionId").selectedOptions[0].text).replaceAll(' ', '%20'))
         this.state.planningUnitLabels.map(ele =>
             csvRow.push((i18n.t('static.planningunit.planningunit')).replaceAll(' ', '%20') + ' , ' + ((ele.toString()).replaceAll(',', '%20')).replaceAll(' ', '%20')))
-        csvRow.push(i18n.t('static.program.isincludeplannedshipment') + ' , ' + (document.getElementById("isPlannedShipmentId").selectedOptions[0].text).replaceAll(' ', '%20'))
+        csvRow.push(i18n.t('static.program.isincludeplannedshipment').replaceAll(' ', '%20') + ' , ' + (document.getElementById("isPlannedShipmentId").selectedOptions[0].text).replaceAll(' ', '%20'))
         csvRow.push('')
         csvRow.push('')
         csvRow.push('')
--- src/views/Report/ShipmentGlobalView.js
+++ src/views/Report/ShipmentGlobalView.js
@@ -1563,10 +1563,17 @@ class ShipmentGlobalView extends Component {
 
         if (this.state.table1Body.length > 0) {
             var A = [];
-            A[0] = this.state.table1Headers;
+
+            let tableHead = this.state.table1Headers;
+            let tableHeadTemp = [];
+
+            for (var i = 0; i < tableHead.length; i++) {
+                tableHeadTemp.push((tableHead[i].replaceAll(',', ' ')).replaceAll(' ', '%20'));
+            }
+            A[0] = tableHeadTemp;
             re = this.state.table1Body
             for (var item = 0; item < re.length; item++) {
-                A.push([[getLabelText(re[item].country.label, this.state.lang), re[item].amount]])
+                A.push([[(getLabelText(re[item].country.label, this.state.lang).replaceAll(',', ' ')).replaceAll(' ', '%20'), re[item].amount]])
             }
             for (var i = 0; i < A.length; i++) {
                 csvRow.push(A[i].join(","))
@@ -1583,10 +1590,10 @@ class ShipmentGlobalView extends Component {
             } else {
                 tempLabel = i18n.t('static.procurementagent.procurementagent');
             }
-            var B = [[i18n.t('static.dashboard.months'), i18n.t('static.program.realmcountry'), i18n.t('static.supplyPlan.amountInUSD'), tempLabel, i18n.t('static.common.status')]];
+            var B = [[(i18n.t('static.dashboard.months').replaceAll(',', ' ')).replaceAll(' ', '%20'), (i18n.t('static.program.realmcountry').replaceAll(',', ' ')).replaceAll(' ', '%20'), (i18n.t('static.supplyPlan.amountInUSD').replaceAll(',', ' ')).replaceAll(' ', '%20'), (tempLabel.replaceAll(',', ' ')).replaceAll(' ', '%20'), (i18n.t('static.common.status').replaceAll(',', ' ')).replaceAll(' ', '%20')]];
             re = this.state.shipmentList;
             for (var item = 0; item < re.length; item++) {
-                B.push([[moment(re[item].transDate, 'YYYY-MM-dd').format('MMM YYYY'), getLabelText(re[item].country.label, this.state.lang), re[item].amount, getLabelText(re[item].fundingSourceProcurementAgent.label, this.state.lang), getLabelText(re[item].shipmentStatus.label, this.state.lang)]])
+                B.push([[(moment(re[item].transDate, 'YYYY-MM-dd').format('MMM YYYY').replaceAll(',', ' ')).replaceAll(' ', '%20'), (getLabelText(re[item].country.label, this.state.lang).replaceAll(',', ' ')).replaceAll(' ', '%20'), re[item].amount, (getLabelText(re[item].fundingSourceProcurementAgent.label, this.state.lang).replaceAll(',', ' ')).replaceAll(' ', '%20'), (getLabelText(re[item].shipmentStatus.label, this.state.lang).replaceAll(',', ' ')).replaceAll(' ', '%20')]])
             }
             for (var i = 0; i < B.length; i++) {
                 csvRow.push(B[i].join(","))
@@ -1679,11 +1686,14 @@ class ShipmentGlobalView extends Component {
                     })
                     var viewby = document.getElementById("viewById").value;
                     if (viewby == 1) {
-                        var planningText = doc.splitTextToSize(i18n.t('static.budget.fundingsource') + ' : ' + this.state.fundingSourceLabels.toString(), doc.internal.pageSize.width * 3 / 4);
-                        doc.text(doc.internal.pageSize.width / 8, 190, planningText)
+
+                        var fundingSourceText = doc.splitTextToSize((i18n.t('static.budget.fundingsource') + ' : ' + this.state.fundingSourceLabels.join('; ')), doc.internal.pageSize.width * 3 / 4);
+                        doc.text(doc.internal.pageSize.width / 8, 190, fundingSourceText)
+
                     } else {
-                        var planningText = doc.splitTextToSize(i18n.t('static.procurementagent.procurementagent') + ' : ' + this.state.procurementAgentLabels.toString(), doc.internal.pageSize.width * 3 / 4);
-                        doc.text(doc.internal.pageSize.width / 8, 190, planningText)
+
+                        var procurementAgentText = doc.splitTextToSize((i18n.t('static.procurementagent.procurementagent') + ' : ' + this.state.procurementAgentLabels.join('; ')), doc.internal.pageSize.width * 3 / 4);
+                        doc.text(doc.internal.pageSize.width / 8, 190, procurementAgentText)
                     }
 
                 }
@@ -2506,7 +2516,7 @@ class ShipmentGlobalView extends Component {
                     this.setState({ message: message })
                 }} />
                 <h6 className="mt-success">{i18n.t(this.props.match.params.message)}</h6>
-                <h5  className="red">{i18n.t(this.state.message)}</h5>
+                <h5 className="red">{i18n.t(this.state.message)}</h5>
 
                 <Card>
                     <div className="Card-header-reporticon">
