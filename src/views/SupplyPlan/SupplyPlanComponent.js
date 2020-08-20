529e11370928f6f3e1ff55dffdf1d3944c674dc1
palash-nikam-altius <palash.n@altius.cc>
2020-08-20 09:23:18 +0530

    commit by palash

--- src/views/SupplyPlan/SupplyPlanComponent.js
+++ src/views/SupplyPlan/SupplyPlanComponent.js
@@ -1418,6 +1418,9 @@ export default class SupplyPlanComponent extends React.Component {
                     }
                 }
                 console.log("ProList", proList);
+                this.setState({
+                    programList: proList,
+                })
                 var programIdd = this.props.match.params.programId;
                 console.log("programIdd", programIdd);
                 if (programIdd != '' && programIdd != undefined) {
@@ -1428,9 +1431,7 @@ export default class SupplyPlanComponent extends React.Component {
                     })
                     this.getPlanningUnitList(programSelect);
                 }
-                this.setState({
-                    programList: proList,
-                })
+               
             }.bind(this);
         }.bind(this);
         console.log("After component did mount");
@@ -1539,15 +1540,6 @@ export default class SupplyPlanComponent extends React.Component {
                                     }
                                 }
                             }
-                            var planningUnitIdProp = this.props.match.params.planningUnitId;
-                            if (planningUnitIdProp != '' && planningUnitIdProp!=undefined) {
-                                var planningUnit = { value: planningUnitIdProp, label: proList.filter(c => c.value == planningUnitIdProp)[0].label };
-                                this.setState({
-                                    planningUnit: planningUnit,
-                                    planningUnitId: planningUnitIdProp
-                                })
-                                this.formSubmit(planningUnit, this.state.monthCount);
-                            }
                             this.setState({
                                 planningUnitList: proList,
                                 programPlanningUnitList: myResult,
@@ -1556,6 +1548,17 @@ export default class SupplyPlanComponent extends React.Component {
                                 dataSourceListAll: dataSourceListAll,
                                 planningUnitListForConsumption: planningUnitListForConsumption
                             })
+                            var planningUnitIdProp = this.props.match.params.planningUnitId;
+                            console.log("planningUnitIdProp===>",planningUnitIdProp);
+                            if (planningUnitIdProp != '' && planningUnitIdProp != undefined) {
+                                var planningUnit = { value: planningUnitIdProp, label: proList.filter(c => c.value == planningUnitIdProp)[0].label };
+                                this.setState({
+                                    planningUnit: planningUnit,
+                                    planningUnitId: planningUnitIdProp
+                                })
+                                this.formSubmit(planningUnit, this.state.monthCount);
+                            }
+
                         }.bind(this);
                     }.bind(this);
                 }.bind(this);
