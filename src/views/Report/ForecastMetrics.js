0e4562381c62a90d490063e360ff28ec0dbe6379
shubham deulkar <shubham.d@altius.cc>
2020-08-24 14:23:17 +0530

    commit by shubham

--- src/views/Report/ForecastMetrics.js
+++ src/views/Report/ForecastMetrics.js
@@ -1032,6 +1032,7 @@ import ProgramService from '../../api/ProgramService';
 import MultiSelect from "react-multi-select-component";
 import jexcel from 'jexcel';
 import "../../../node_modules/jexcel/dist/jexcel.css";
+import { contrast } from "../../CommonComponent/JavascriptCommonFunctions";
 import { jExcelLoadedFunction, jExcelLoadedFunctionOnlyHideRow } from '../../CommonComponent/JExcelCommonFunctions.js'
 
 // const { getToggledOptions } = utils;
@@ -1439,6 +1440,8 @@ class ForecastMetrics extends Component {
           for (var i = 0; i < colArr.length; i++) {
             elInstance.setStyle(`${colArr[i]}${parseInt(y) + 1}`, 'background-color', 'transparent');
             elInstance.setStyle(`${colArr[i]}${parseInt(y) + 1}`, 'background-color', '#f48282');
+            let textColor = contrast('#f48282');
+            elInstance.setStyle(`${colArr[i]}${parseInt(y) + 1}`, 'color', textColor);
           }
         } else {
           for (var i = 0; i < colArr.length; i++) {
