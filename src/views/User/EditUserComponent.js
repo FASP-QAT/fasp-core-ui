69a728d4df7fac698db86235de2392ee8792aeb1
anchal-chouvhan-altius <anchal.c@altius.cc>
2020-09-11 18:12:58 +0530

    Commited by anchal

--- src/views/User/EditUserComponent.js
+++ src/views/User/EditUserComponent.js
@@ -45,8 +45,6 @@ const validationSchema = function (values) {
         roleId: Yup.string()
             .test('roleValid', i18n.t('static.common.roleinvalidtext'),
                 function (value) {
-                    console.log("value---", value);
-                    console.log("condition---", document.getElementById("roleValid").value);
                     if (document.getElementById("roleValid").value == "false") {
                         console.log("inside if ---", value);
                         return true;
@@ -227,6 +225,7 @@ class EditUserComponent extends Component {
     }
     componentDidMount() {
         AuthenticationService.setupAxiosInterceptors();
+        document.getElementById("roleValid").value = false;
         // console.log("USERID --> ", this.props.match.params.userId);
         UserService.getUserByUserId(this.props.match.params.userId).then(response => {
             if (response.status == 200) {
