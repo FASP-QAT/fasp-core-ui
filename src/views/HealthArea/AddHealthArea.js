b19735b351d9d77b3086148e7c61a57497561ce2
anchal-chouvhan-altius <anchal.c@altius.cc>
2020-09-18 13:06:26 +0530

    Commited by anchal

--- src/views/HealthArea/AddHealthArea.js
+++ src/views/HealthArea/AddHealthArea.js
@@ -13,6 +13,7 @@ import AuthenticationService from '../Common/AuthenticationService.js';
 import getLabelText from '../../CommonComponent/getLabelText';
 import AuthenticationServiceComponent from '../Common/AuthenticationServiceComponent';
 import classNames from 'classnames';
+import { ALPHABET_NUMBER_REGEX, SPACE_REGEX } from '../../Constants.js';
 
 const entityname = i18n.t('static.healtharea.healtharea');
 
@@ -27,11 +28,13 @@ const validationSchema = function (values) {
     realmId: Yup.string()
       .required(i18n.t('static.common.realmtext')),
     healthAreaName: Yup.string()
-      .matches(/^([a-zA-Z]+\s)*[a-zA-Z]+$/, i18n.t('static.message.rolenamevalidtext'))
+      // .matches(/^([a-zA-Z]+\s)*[a-zA-Z]+$/, i18n.t('static.message.rolenamevalidtext'))
+      .matches(SPACE_REGEX, i18n.t('static.common.spacenotallowed'))
       .required(i18n.t('static.healtharea.healthareatext')),
     healthAreaCode: Yup.string()
-      .max(3, 'Technical Area Code Is 3 Digit')
-      .required(i18n.t('static.country.countrycodetext')),
+      .matches(ALPHABET_NUMBER_REGEX, i18n.t('static.message.alphabetnumerallowed'))
+      .max(6, 'Technical Area Code length should be 6')
+      .required(i18n.t('static.common.displayName')),
     realmCountryId: Yup.string()
       .required(i18n.t('static.program.validcountrytext'))
 
