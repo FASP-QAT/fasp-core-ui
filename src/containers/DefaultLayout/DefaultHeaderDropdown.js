ee65c4b7b15923c14e503ab3122cb021b4dfac1f
anchal-chouvhan-altius <anchal.c@altius.cc>
2020-10-06 23:51:01 +0530

    Commited by anchal

--- src/containers/DefaultLayout/DefaultHeaderDropdown.js
+++ src/containers/DefaultLayout/DefaultHeaderDropdown.js
@@ -75,6 +75,7 @@ class DefaultHeaderDropdown extends Component {
   }
 
   changeLanguage(lang) {
+    console.log("Going to change language---",lang)
     localStorage.setItem('lang', lang);
     AuthenticationService.updateUserLanguage(lang);
     if (navigator.onLine) {
@@ -98,7 +99,12 @@ class DefaultHeaderDropdown extends Component {
             });
           })
     }
+    else{
+      console.log("Going to change language you are offline---")
+    }
+    console.log("Going to change language call changeLanguage function---")
     i18n.changeLanguage(lang)
+    console.log("Going to change language reload location reload---")
     window.location.reload();
 
   }
