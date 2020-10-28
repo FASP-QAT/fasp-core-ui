8f39a4987534bff6c4b6e7aebc9fc75e6146204d
junaid sheikh <junaid.s@altius.cc>
2020-10-28 22:54:17 +0530

    commited by junaid

--- src/views/ProcurementUnit/ListProcurementUnit.js
+++ src/views/ProcurementUnit/ListProcurementUnit.js
@@ -429,7 +429,7 @@ export default class ListProcurementUnit extends Component {
     var options = {
       data: data,
       columnDrag: true,
-      colWidths: [150, 150, 100],
+      colWidths: [150, 150, 100,50,50,150],
       colHeaderClasses: ["Reqasterisk"],
       columns: [
         {
