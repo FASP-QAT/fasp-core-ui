d7b011c2e08752664bf75ab72bb439dba613d80c
palash-nikam-altius <palash.n@altius.cc>
2020-11-09 18:23:55 +0530

    commit by palash on shubham branch

--- src/views/Pipeline/PipelineProgramDataStepTwo.js
+++ src/views/Pipeline/PipelineProgramDataStepTwo.js
@@ -96,7 +96,8 @@ export default class PipelineProgramDataStepTwo extends Component {
     // }
     componentDidMount() {
         // AuthenticationService.setupAxiosInterceptors();
-        ProgramService.getRealmCountryList(1)
+        var realmId = AuthenticationService.getRealmId();
+        ProgramService.getRealmCountryList(realmId)
             .then(response => {
                 if (response.status == 200) {
                     this.setState({
