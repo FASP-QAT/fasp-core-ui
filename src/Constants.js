1582df002243724e974ba81df7735ff34c7f42a6
Ekta <ekta.n@altius.cc>
2020-11-18 09:06:54 +0000

    Parameters for Production

--- src/Constants.js
+++ src/Constants.js
@@ -3,7 +3,7 @@
 //export const API_URL = 'http://localhost:8081'
 // export const API_URL = 'http://server6.altius.cc:8080/FASP'
 
-export const API_URL = 'https://github.com/AltiusIndia/fasp-core-ui.git'
+export const API_URL = 'https://api.quantificationanalytics.org'
 // export const API_URL='http://192.168.43.70:8080/FASP'
 export const SECRET_KEY = 'afhghggrehreqiuhrjhjdbfjehgfuoerhfjhfyguayaegrf'
 export const MONTHS_IN_PAST_FOR_SUPPLY_PLAN = 5;
