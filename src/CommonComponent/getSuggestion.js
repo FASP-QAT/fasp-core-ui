ec063725a058edbf0c7dbb12f6844bcf22d6a9fb
palash-nikam-altius <palash.n@altius.cc>
2020-11-25 11:32:04 +0530

    commit by palash

--- src/CommonComponent/getSuggestion.js
+++ src/CommonComponent/getSuggestion.js
@@ -330,6 +330,37 @@ export default function getSuggestion(row, lang) {
 
         return getLabelText(label, lang);
     }
+    if (row.realmProblem.problem.problemId == 14) {
+        // var label = row.realmProblem.problem.label;
+        // label.label_en = "Please fill proper notes in shipments for " + row.planningUnit.label.label_en;
+        // // + monthString.replace(/,\s*$/, "");
+        // return getLabelText(label, lang);
+        // var desc = row.realmProblem.problem.actionLabel.label_en;
+        // const result = desc.split('<%PLANNING_UNIT%>').join(row.planningUnit.label.label_en);
+        // var label = row.realmProblem.problem.actionLabel;
+        // label.label_en = result;
+        var desc_en = row.realmProblem.problem.actionLabel.label_en;
+        var desc_fr = row.realmProblem.problem.actionLabel.label_fr;
+        var desc_sp = row.realmProblem.problem.actionLabel.label_sp;
+        var desc_pr = row.realmProblem.problem.actionLabel.label_pr;
+
+        var label = row.realmProblem.problem.actionLabel;
+        if (desc_en != null && desc_en != '') {
+            const result_en = desc_en.split('<%PLANNING_UNIT%>').join(getLabelText(row.planningUnit.label, lang));
+            label.label_en = result_en;
+        } if (desc_fr != null && desc_fr != '') {
+            const result_fr = desc_fr.split('<%PLANNING_UNIT%>').join(getLabelText(row.planningUnit.label, lang));
+            label.label_fr = result_fr;
+        } if (desc_sp != null && desc_sp != '') {
+            const result_sp = desc_sp.split('<%PLANNING_UNIT%>').join(getLabelText(row.planningUnit.label, lang));
+            label.label_sp = result_sp;
+        } if (desc_pr != null && desc_pr != '') {
+            const result_pr = desc_pr.split('<%PLANNING_UNIT%>').join(getLabelText(row.planningUnit.label, lang));
+            label.label_pr = result_pr;
+        }
+
+        return getLabelText(label, lang);
+    }
     if (row.realmProblem.problem.problemId == 16) {
         // var desc = row.realmProblem.problem.actionLabel.label_en;
         // const result = desc.split('<%PLANNING_UNIT%>').join(row.planningUnit.label.label_en).split('<%DT%>').join(moment(row.dt).format('MMM-YY'));
