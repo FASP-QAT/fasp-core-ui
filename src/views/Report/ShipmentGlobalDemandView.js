47db2de04981dabda3ea535678f200a8508e040a
ekta-nalawade-altius <61678857+ekta-nalawade-altius@users.noreply.github.com>
2020-12-22 00:14:46 +0530

    Update ShipmentGlobalDemandView.js

--- src/views/Report/ShipmentGlobalDemandView.js
+++ src/views/Report/ShipmentGlobalDemandView.js
@@ -1792,12 +1792,13 @@ class ShipmentGlobalDemandView extends Component {
                 // AuthenticationService.setupAxiosInterceptors();
                 let productCategoryId = document.getElementById("productCategoryId").value;
                 // AuthenticationService.setupAxiosInterceptors();
+               var lang=this.state.lang
                 if (productCategoryId != -1) {
                     PlanningUnitService.getActivePlanningUnitByProductCategoryId(productCategoryId).then(response => {
                         // console.log("PLANNING-UNIT--->", response.data);
-                        response.data.sort(function (a, b) {
-                            return a.localeCompare(b); //using String.prototype.localCompare()
-                          });
+                         (response.data).sort(function (a, b) {
+                    return getLabelText(a.label, lang).localeCompare(getLabelText(b.label, lang)); //using String.prototype.localCompare()
+                  });
                         this.setState({
                             planningUnits: response.data,
                         }, () => {
