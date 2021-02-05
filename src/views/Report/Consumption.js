e28364977f5db2580d09a3bfe3449fdef771f406
shubham deulkar <shubham.d@altius.cc>
2021-02-05 15:23:34 +0530

    commit by shubham

--- src/views/Report/Consumption.js
+++ src/views/Report/Consumption.js
@@ -112,7 +112,7 @@ class Consumption extends Component {
       show: false,
       message: '',
       rangeValue: { from: { year: dt.getFullYear(), month: dt.getMonth() }, to: { year: new Date().getFullYear(), month: new Date().getMonth() + 1 } },
-      minDate: { year: new Date().getFullYear() - 3, month: new Date().getMonth() + 2 },
+      minDate: { year: new Date().getFullYear() - 10, month: new Date().getMonth() + 2 },
       maxDate: { year: new Date().getFullYear() + 3, month: new Date().getMonth() },
       loading: true,
       programId: '',
