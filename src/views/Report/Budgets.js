375bcc1812904a7faafbda43725b866c5edaeaca
shubham deulkar <shubham.d@altius.cc>
2021-04-05 09:26:16 +0530

    commit by shubham

--- src/views/Report/Budgets.js
+++ src/views/Report/Budgets.js
@@ -141,7 +141,7 @@ class Budgets extends Component {
             loading: true,
             rangeValue: { from: { year: dt.getFullYear(), month: dt.getMonth() }, to: { year: new Date().getFullYear(), month: new Date().getMonth() + 1 } },
             minDate: { year: new Date().getFullYear() - 10, month: new Date().getMonth() + 2 },
-            maxDate: { year: new Date().getFullYear() + 3, month: new Date().getMonth() },
+            maxDate: { year: new Date().getFullYear() + 10, month: new Date().getMonth() },
             fundingSourceValues: [],
             fundingSourceLabels: [],
             fundingSources: [],
