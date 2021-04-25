a5e93c39044350746512cd7811a212688d6a97e3
dolly-chhabriya-altius <dolly.c@altius.cc>
2021-04-26 00:53:10 +0530

    Console in supply plan

--- src/views/SupplyPlan/SupplyPlanComponent.js
+++ src/views/SupplyPlan/SupplyPlanComponent.js
@@ -2329,6 +2329,7 @@ export default class SupplyPlanComponent extends React.Component {
                         var myResult = [];
                         var programId = (value != "" && value != undefined ? value.value : 0).split("_")[0];
                         myResult = planningunitRequest.result.filter(c => c.program.id == programId);
+                        console.log("MyResult1++++",myResult);
                         var proList = []
                         for (var i = 0; i < myResult.length; i++) {
                             if (myResult[i].program.id == programId && myResult[i].active == true) {
@@ -2340,7 +2341,8 @@ export default class SupplyPlanComponent extends React.Component {
                                 planningList.push(myResult[i]);
                             }
                         }
-
+console.log("ProList+++",proList);
+console.log("PlanningList+++",planningList)
                         var puTransaction = db1.transaction(['planningUnit'], 'readwrite');
                         var puOs = puTransaction.objectStore('planningUnit');
                         var puRequest = puOs.getAll();
@@ -2357,7 +2359,7 @@ export default class SupplyPlanComponent extends React.Component {
                             var puResult = [];
                             puResult = puRequest.result;
                             planningUnitListForConsumption = puResult;
-
+                            console.log("PlanningUnitListfor consumotion+++",puResult);
 
                             var dataSourceTransaction = db1.transaction(['dataSource'], 'readwrite');
                             var dataSourceOs = dataSourceTransaction.objectStore('dataSource');
@@ -2381,6 +2383,12 @@ export default class SupplyPlanComponent extends React.Component {
                                         }
                                     }
                                 }
+                                console.log("After Sorting+++",proList.sort(function (a, b) {
+                                    a = a.label.toLowerCase();
+                                    b = b.label.toLowerCase();
+                                    return a < b ? -1 : a > b ? 1 : 0;
+                                }));
+                                console.log("MyResult2+++",myResult);
                                 this.setState({
                                     planningUnitList: proList.sort(function (a, b) {
                                         a = a.label.toLowerCase();
