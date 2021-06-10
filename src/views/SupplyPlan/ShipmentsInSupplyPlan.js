00cf4990236f28707261d0b6b448375a87a2afc6
dolly-chhabriya-altius <dolly.c@altius.cc>
2021-06-10 17:26:26 +0530

    Changes for the shipment dates for ERP

--- src/views/SupplyPlan/ShipmentsInSupplyPlan.js
+++ src/views/SupplyPlan/ShipmentsInSupplyPlan.js
@@ -869,6 +869,7 @@ export default class ShipmentsInSupplyPlanComponent extends React.Component {
                                                                         var shipmentDates = rowData[27];
                                                                         var shipmentStatus = rowData[3];
                                                                         var emergencyOrder = rowData[11];
+                                                                        var erpFlag = rowData[22];
                                                                         var expectedDeliveryDate = shipmentDates.expectedDeliveryDate;
                                                                         var expectedPlannedDate = "";
                                                                         var expectedSubmittedDate = "";
@@ -881,20 +882,22 @@ export default class ShipmentsInSupplyPlanComponent extends React.Component {
                                                                         var shippedDate = shipmentDates.shippedDate;
                                                                         var arrivedDate = shipmentDates.arrivedDate;
                                                                         var receivedDate = shipmentDates.receivedDate;
-                                                                        if (shipmentStatus != DELIVERED_SHIPMENT_STATUS) {
-                                                                            receivedDate = null;
-                                                                        }
-                                                                        if (shipmentStatus != ARRIVED_SHIPMENT_STATUS && shipmentStatus != DELIVERED_SHIPMENT_STATUS) {
-                                                                            arrivedDate = null;
-                                                                        }
-                                                                        if (shipmentStatus != SHIPPED_SHIPMENT_STATUS && shipmentStatus != ARRIVED_SHIPMENT_STATUS && shipmentStatus != DELIVERED_SHIPMENT_STATUS) {
-                                                                            shippedDate = null;
-                                                                        }
-                                                                        if (shipmentStatus != APPROVED_SHIPMENT_STATUS && shipmentStatus != SHIPPED_SHIPMENT_STATUS && shipmentStatus != ARRIVED_SHIPMENT_STATUS && shipmentStatus != DELIVERED_SHIPMENT_STATUS) {
-                                                                            approvedDate = null;
-                                                                        }
-                                                                        if (shipmentStatus != SUBMITTED_SHIPMENT_STATUS && shipmentStatus != APPROVED_SHIPMENT_STATUS && shipmentStatus != SHIPPED_SHIPMENT_STATUS && shipmentStatus != ARRIVED_SHIPMENT_STATUS && shipmentStatus != DELIVERED_SHIPMENT_STATUS) {
-                                                                            submittedDate = null;
+                                                                        if (erpFlag.toString() == "false") {
+                                                                            if (shipmentStatus != DELIVERED_SHIPMENT_STATUS) {
+                                                                                receivedDate = null;
+                                                                            }
+                                                                            if (shipmentStatus != ARRIVED_SHIPMENT_STATUS && shipmentStatus != DELIVERED_SHIPMENT_STATUS) {
+                                                                                arrivedDate = null;
+                                                                            }
+                                                                            if (shipmentStatus != SHIPPED_SHIPMENT_STATUS && shipmentStatus != ARRIVED_SHIPMENT_STATUS && shipmentStatus != DELIVERED_SHIPMENT_STATUS) {
+                                                                                shippedDate = null;
+                                                                            }
+                                                                            if (shipmentStatus != APPROVED_SHIPMENT_STATUS && shipmentStatus != SHIPPED_SHIPMENT_STATUS && shipmentStatus != ARRIVED_SHIPMENT_STATUS && shipmentStatus != DELIVERED_SHIPMENT_STATUS) {
+                                                                                approvedDate = null;
+                                                                            }
+                                                                            if (shipmentStatus != SUBMITTED_SHIPMENT_STATUS && shipmentStatus != APPROVED_SHIPMENT_STATUS && shipmentStatus != SHIPPED_SHIPMENT_STATUS && shipmentStatus != ARRIVED_SHIPMENT_STATUS && shipmentStatus != DELIVERED_SHIPMENT_STATUS) {
+                                                                                submittedDate = null;
+                                                                            }
                                                                         }
                                                                         var addLeadTimes = 0;
                                                                         if (rowData[7].toString() == "true") {
@@ -1040,7 +1043,7 @@ export default class ShipmentsInSupplyPlanComponent extends React.Component {
                                                                             allowExport: false,
                                                                             onchange: this.shipmentDatesChanged,
                                                                             editable: tableEditable,
-                                                                            onbeforepaste:function(obj,data,x,y){
+                                                                            onbeforepaste: function (obj, data, x, y) {
                                                                                 return false;
                                                                             },
                                                                             contextMenu: function (obj, x, y, e) {
@@ -3330,23 +3333,23 @@ export default class ShipmentsInSupplyPlanComponent extends React.Component {
                 var shipmentStatus = elInstance.getRowData(y)[3];
                 if (shipmentStatus != CANCELLED_SHIPMENT_STATUS && shipmentStatus != ON_HOLD_SHIPMENT_STATUS) {
                     // if (shipmentStatus == DELIVERED_SHIPMENT_STATUS || shipmentStatus == SHIPPED_SHIPMENT_STATUS || shipmentStatus == ARRIVED_SHIPMENT_STATUS) {
-                        var totalShipmentQty = (rowData[26]);
-                        var adjustedOrderQty = elInstance.getValue(`K${parseInt(y) + 1}`, true).toString().replaceAll("\,", "");
-                        adjustedOrderQty = adjustedOrderQty.toString().replaceAll("\,", "");
-                        var col = ("K").concat(parseInt(y) + 1);
+                    var totalShipmentQty = (rowData[26]);
+                    var adjustedOrderQty = elInstance.getValue(`K${parseInt(y) + 1}`, true).toString().replaceAll("\,", "");
+                    adjustedOrderQty = adjustedOrderQty.toString().replaceAll("\,", "");
+                    var col = ("K").concat(parseInt(y) + 1);
 
-                        elInstance.setStyle(col, "background-color", "transparent");
-                        elInstance.setStyle(col, "background-color", "yellow");
-                        elInstance.setComments(col, i18n.t('static.supplyPlan.batchNumberMissing'));
-                        inValid("K", y, i18n.t('static.supplyPlan.batchNumberMissing'), elInstance);
-                        if (totalShipmentQty != 0 && totalShipmentQty > adjustedOrderQty) {
-                            valid = false;
-                            elInstance.setValueFromCoords(31, y, 1, true);
-                            this.props.updateState("shipmentBatchError", i18n.t('static.supplyPlan.batchNumberMissing'));
-                            this.props.hideSecondComponent();
-                        } else {
-                            positiveValidation("K", y, elInstance);
-                        }
+                    elInstance.setStyle(col, "background-color", "transparent");
+                    elInstance.setStyle(col, "background-color", "yellow");
+                    elInstance.setComments(col, i18n.t('static.supplyPlan.batchNumberMissing'));
+                    inValid("K", y, i18n.t('static.supplyPlan.batchNumberMissing'), elInstance);
+                    if (totalShipmentQty != 0 && totalShipmentQty > adjustedOrderQty) {
+                        valid = false;
+                        elInstance.setValueFromCoords(31, y, 1, true);
+                        this.props.updateState("shipmentBatchError", i18n.t('static.supplyPlan.batchNumberMissing'));
+                        this.props.hideSecondComponent();
+                    } else {
+                        positiveValidation("K", y, elInstance);
+                    }
                     // }
                 }
             } else {
