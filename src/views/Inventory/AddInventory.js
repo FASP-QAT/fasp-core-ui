619ec9944c794c5a1d095b7f94a5dae06b60710d
shubham deulkar <shubham.d@altius.cc>
2021-07-06 08:07:49 +0530

    resolve negative adjustment issue in tempalate

--- src/views/Inventory/AddInventory.js
+++ src/views/Inventory/AddInventory.js
@@ -191,8 +191,8 @@ export default class AddInventory extends Component {
                 type: 'whole',
                 operator: 'greaterThan',
                 showErrorMessage: true,
-                // formulae: [-1],
-                formulae: [-100000000],
+                formulae: [-1],
+                // formulae: [-100000000],
                 // errorStyle: 'error',
                 // errorTitle: 'Invalid Value',
                 // error: 'Invalid Value'
