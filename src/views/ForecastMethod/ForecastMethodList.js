7de9b220ff606538b59823e556d77ae5381eca04
Anisha Selote <Anisha.s@altius.cc>
2021-09-28 16:23:30 +0530

    commited by anisha

--- src/views/ForecastMethod/ForecastMethodList.js
+++ src/views/ForecastMethod/ForecastMethodList.js
@@ -878,7 +878,7 @@ class forecastMethod extends Component {
         return (
             <div className="animated fadeIn">
                 <AuthenticationServiceComponent history={this.props.history} />
-                <h5 style={{ color: "red" }}>{i18n.t('static.common.customWarningMessage')}</h5>
+                {/* <h5 style={{ color: "red" }}>{i18n.t('static.common.customWarningMessage')}</h5> */}
                 <h5>{i18n.t(this.props.match.params.message, { entityname })}</h5>
                 {/* <h5 style={{ color: "red" }} id="div2">{i18n.t(this.state.message, { entityname })}</h5> */}
                 <h5 style={{ color: this.state.color }} id="div2">{this.state.message}</h5>
@@ -886,8 +886,8 @@ class forecastMethod extends Component {
                     <CardBody className="p-0">
 
                         <Col xs="12" sm="12">
-
-                            <div id="paputableDiv" style={{ display: this.state.loading ? "none" : "block" }} className={(AuthenticationService.getLoggedInUserRoleBusinessFunctionArray().includes('ROLE_BF_EDIT_FORECAST_METHOD') || AuthenticationService.getLoggedInUserRoleBusinessFunctionArray().includes('ROLE_BF_ADD_FORECAST_METHOD')) ? "RowClickable" : "jexcelremoveReadonlybackground"}>
+                        <h5 style={{ color: "red" }} >{i18n.t('static.common.customWarningMessage')}</h5>
+                            <div id="paputableDiv" style={{ display: this.state.loading ? "none" : "block",marginTop:'-13px' }} className={(AuthenticationService.getLoggedInUserRoleBusinessFunctionArray().includes('ROLE_BF_EDIT_FORECAST_METHOD') || AuthenticationService.getLoggedInUserRoleBusinessFunctionArray().includes('ROLE_BF_ADD_FORECAST_METHOD')) ? "RowClickable" : "jexcelremoveReadonlybackground"}>
                             </div>
                             <div style={{ display: this.state.loading ? "block" : "none" }}>
                                 <div className="d-flex align-items-center justify-content-center" style={{ height: "500px" }} >
