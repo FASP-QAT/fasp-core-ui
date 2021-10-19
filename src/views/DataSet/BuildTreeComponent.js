1b7d3278330d6fb4af9f2abe405254d1a3832f5d
palash nikam <palash.n@altius.cc>
2021-10-19 12:15:17 +0530

    commit by palash

--- src/views/DataSet/BuildTreeComponent.js
+++ src/views/DataSet/BuildTreeComponent.js
@@ -177,16 +177,7 @@ const Node = ({ itemConfig, isDragging, connectDragSource, canDrop, isOver, conn
 function addCommas(cell1, row) {
 
     if (cell1 != null && cell1 != "") {
-        var cell = parseFloat(cell1);
-        cell += '';
-        var x = cell.split('.');
-        var x1 = x[0];
-        var x2 = x.length > 1 ? '.' + x[1] : '';
-        var rgx = /(\d+)(\d{3})/;
-        while (rgx.test(x1)) {
-            x1 = x1.replace(rgx, '$1' + ',' + '$2');
-        }
-        return x1 + x2;
+        return cell1.toString().replaceAll(",", "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
     } else {
         return "";
     }
