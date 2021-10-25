c2f738ba9de5f7aaa0ce0476810e47f47e151c3e
shubham deulkar <shubham.d@altius.cc>
2021-10-25 08:41:46 +0530

    add calculator changes

--- src/containers/DefaultLayout/DefaultLayout.js
+++ src/containers/DefaultLayout/DefaultLayout.js
@@ -1072,6 +1072,9 @@ class DefaultLayout extends Component {
     // console.log("User111-----", decryptedUser1.defaultModuleId);
     this.setState({
       activeTab: decryptedUser1.defaultModuleId,
+    }, () => {
+      let id = AuthenticationService.displayDashboardBasedOnRole();
+      this.props.history.push(`/ApplicationDashboard/` + `${id}`);
     });
 
   }
--- src/views/UsageTemplate/UsageTemplateList.js
+++ src/views/UsageTemplate/UsageTemplateList.js
@@ -56,8 +56,9 @@ const validationSchema = function (values) {
         //     .required(i18n.t('static.datasource.datasourcetext')),
         number1: Yup.string()
             .matches(/^\d{0,2}(\.\d{1,2})?$/, i18n.t('static.message.2digitDecimal'))
+            .matches(/^(?=.*[1-9])\d{1,10}$/, i18n.t('static.program.validvaluetext'))
             .required(i18n.t('static.label.fieldRequired'))
-            .min(0, i18n.t('static.program.validvaluetext'))
+            // .min(1, i18n.t('static.program.validvaluetext'))
     })
 }
 
@@ -1190,15 +1191,39 @@ class usageTemplate extends Component {
                                 title: i18n.t('static.usageTemplate.calculateUsageFrequency'),
                                 onclick: function () {
                                     // console.log("onclick------>", this.el.getValueFromCoords(0, y));
+                                    let value = this.el.getValueFromCoords(17, y);
+                                    let tempUsagePeriodList = [];
+
+                                    if (typeof value === 'number') {
+                                        //it's a number
+
+                                        let tempList = this.state.usagePeriodListLong;
+                                        let selectedPickerConvertTOMonth = tempList.filter(c => c.usagePeriodId == value)[0].convertToMonth;
+
+
+                                        for (var i = 0; i < tempList.length; i++) {
+                                            if (parseFloat(tempList[i].convertToMonth) <= parseFloat(selectedPickerConvertTOMonth)) {
+                                                tempUsagePeriodList.push(tempList[i]);
+                                            }
+                                        }
+
+                                        // this.setState({
+                                        //     usagePeriodDisplayList: tempUsagePeriodList,
+                                        // }, () => { });
+
+                                    }
+
+
+
                                     this.setState({
                                         isModalOpen: true,
                                         x: x,
                                         y: y,
-                                        number1: '',
-                                        number2: '',
-                                        picker1: '',
-                                        picker2: '',
-                                        usagePeriodDisplayList: []
+                                        number1: (this.el.getValueFromCoords(15, y) != '' ? this.el.getValueFromCoords(15, y) : ''),
+                                        number2: (this.el.getValueFromCoords(15, y) != '' ? this.el.getValueFromCoords(15, y) : ''),
+                                        picker1: (this.el.getValueFromCoords(17, y) != '' ? this.el.getValueFromCoords(17, y) : ''),
+                                        picker2: (this.el.getValueFromCoords(17, y) != '' ? this.el.getValueFromCoords(17, y) : ''),
+                                        usagePeriodDisplayList: (tempUsagePeriodList.length == 0 ? [] : tempUsagePeriodList)
                                     })
 
                                 }.bind(this)
@@ -2548,7 +2573,12 @@ class usageTemplate extends Component {
                                 <Card>
                                     <Formik
                                         enableReinitialize={true}
-                                        initialValues={initialValues}
+                                        initialValues={{
+                                            number1: this.state.number1,
+                                            picker1: this.state.picker1,
+                                            number2: this.state.number2,
+                                            picker2: this.state.picker2,
+                                        }}
                                         validate={validate(validationSchema)}
                                         onSubmit={(values, { setSubmitting, setErrors }) => {
 
@@ -2704,7 +2734,7 @@ class usageTemplate extends Component {
                                                         {(this.state.roleArray.includes('ROLE_REALM_ADMIN') || this.state.roleArray.includes('ROLE_DATASET_ADMIN')) &&
                                                             <FormGroup>
                                                                 <Button type="button" color="danger" className="mr-1 float-right" size="md" onClick={this.modelOpenClose}><i className="fa fa-times"></i> {i18n.t('static.common.cancel')}</Button>
-                                                                <Button type="submit" color="success" className="mr-1 float-right" size="md" onClick={() => this.touchAll(setTouched, errors)} disabled={!isValid}><i className="fa fa-check"></i>{i18n.t('static.common.submit')}</Button>
+                                                                <Button type="submit" color="success" className="mr-1 float-right" size="md" onClick={() => this.touchAll(setTouched, errors)}><i className="fa fa-check"></i>{i18n.t('static.common.submit')}</Button>
                                                                 &nbsp;
 
                                                             </FormGroup>
@@ -2787,7 +2817,8 @@ class usageTemplate extends Component {
             this.setState({
                 picker1: event.target.value,
                 usagePeriodDisplayList: tempUsagePeriodList,
-                number2: number2
+                number2: number2,
+                picker2: ''
             }, () => { });
         } else if (event.target.name == "picker2") {
             let tempList = this.state.usagePeriodListLong;
