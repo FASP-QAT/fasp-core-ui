c299fa1f1eca7c9eba5c6d4b2e9a4089616cc32a
Anisha Selote <Anisha.s@altius.cc>
2021-11-12 11:06:11 +0530

    commited by anisha

--- src/views/DataSet/ListTreeComponent.js
+++ src/views/DataSet/ListTreeComponent.js
@@ -387,6 +387,7 @@ export default class ListTreeComponent extends Component {
                                                             name="templateId"
                                                             id="templateId"
                                                             bsSize="sm"
+                                                            className="addtreebg"
                                                             onChange={(e) => { this.onTemplateChange(e) }}
                                                         >
                                                             <option value="">{'+ Add Tree'}</option>
