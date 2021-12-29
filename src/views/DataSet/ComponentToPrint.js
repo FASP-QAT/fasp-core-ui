029aebe02606b4a1b1cac6ff04fe3bc8cbf76ad0
Shrutika <shrutika.k@altius.cc>
2021-12-29 15:37:25 +0530

    popup print

---
+++ src/views/DataSet/ComponentToPrint.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// export class ComponentToPrint extends React.PureComponent {
+//     render() {
+//         return (
+//             <div>My cool content here!</div>
+//         );
+//     }
+// }
+
