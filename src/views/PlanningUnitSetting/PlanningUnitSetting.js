87ec3d69f167ff32f373eaf278c968416d43d931
shubham deulkar <shubham.d@altius.cc>
2022-01-13 20:08:08 +0530

    commit by shubham

--- src/views/PlanningUnitSetting/PlanningUnitSetting.js
+++ src/views/PlanningUnitSetting/PlanningUnitSetting.js
@@ -922,22 +922,21 @@ export default class PlanningUnitSetting extends Component {
             var tempId = sel.options[sel.selectedIndex].text;
             let forecastProgramVersionId = tempId.split('~')[1];
             let selectedForecastProgram = this.state.datasetList.filter(c => c.programId == pID && c.versionId == forecastProgramVersionId)[0]
-            let startDateSplit = selectedForecastProgram.forecastStartDate.split('-');
-            let stopDateSplit = selectedForecastProgram.forecastStopDate.split('-');
 
-            let forecastStopDate = new Date(selectedForecastProgram.forecastStartDate);
-            forecastStopDate.setMonth(forecastStopDate.getMonth() - 1);
+            let forecastStartDate = selectedForecastProgram.forecastStartDate;
+            let forecastStopDate = selectedForecastProgram.forecastStopDate;
 
-            let d1 = new Date(startDateSplit[1] - 3 + '-' + (new Date(selectedForecastProgram.forecastStartDate).getMonth() + 1) + '-01 00:00:00');
-            d1.setMonth(d1.getMonth() - 1);
+            let beforeEndDateDisplay = new Date(selectedForecastProgram.forecastStartDate);
+            beforeEndDateDisplay.setMonth(beforeEndDateDisplay.getMonth() - 1);
 
             this.setState(
                 {
                     datasetId: pID,
-                    rangeValue: { from: { year: startDateSplit[1] - 3, month: new Date(selectedForecastProgram.forecastStartDate).getMonth() + 1 }, to: { year: forecastStopDate.getFullYear(), month: forecastStopDate.getMonth() + 1 } },
-                    startDateDisplay: months[new Date(selectedForecastProgram.forecastStartDate).getMonth()] + ' ' + (startDateSplit[1] - 3),
-                    endDateDisplay: months[(forecastStopDate.getMonth())] + ' ' + forecastStopDate.getFullYear(),
-                    beforeEndDateDisplay: months[(d1.getMonth())] + ' ' + d1.getFullYear(),
+                    // rangeValue: { from: { year: startDateSplit[1] - 3, month: new Date(selectedForecastProgram.forecastStartDate).getMonth() + 1 }, to: { year: forecastStopDate.getFullYear(), month: forecastStopDate.getMonth() + 1 } },
+                    rangeValue: { from: { year: new Date(forecastStartDate).getFullYear(), month: new Date(forecastStartDate).getMonth() + 1 }, to: { year: new Date(forecastStopDate).getFullYear(), month: new Date(forecastStopDate).getMonth() + 1 } },
+                    startDateDisplay: months[new Date(forecastStartDate).getMonth()] + ' ' + new Date(forecastStartDate).getFullYear(),
+                    endDateDisplay: months[new Date(forecastStopDate).getMonth()] + ' ' + new Date(forecastStopDate).getFullYear(),
+                    beforeEndDateDisplay: months[new Date(beforeEndDateDisplay).getMonth()] + ' ' + new Date(beforeEndDateDisplay).getFullYear(),
                 }, () => {
                     // console.log("d----------->0", d1);
                     // console.log("d----------->00", (d1.getMonth()));
@@ -965,6 +964,57 @@ export default class PlanningUnitSetting extends Component {
 
     }
 
+    // setProgramId(event) {
+    //     // console.log("PID----------------->", document.getElementById("forecastProgramId").value);
+    //     var pID = document.getElementById("forecastProgramId").value;
+    //     if (pID != 0) {
+    //         var sel = document.getElementById("forecastProgramId");
+    //         var tempId = sel.options[sel.selectedIndex].text;
+    //         let forecastProgramVersionId = tempId.split('~')[1];
+    //         let selectedForecastProgram = this.state.datasetList.filter(c => c.programId == pID && c.versionId == forecastProgramVersionId)[0]
+    //         let startDateSplit = selectedForecastProgram.forecastStartDate.split('-');
+    //         let stopDateSplit = selectedForecastProgram.forecastStopDate.split('-');
+
+    //         let forecastStopDate = new Date(selectedForecastProgram.forecastStartDate);
+    //         forecastStopDate.setMonth(forecastStopDate.getMonth() - 1);
+
+    //         let d1 = new Date(startDateSplit[1] - 3 + '-' + (new Date(selectedForecastProgram.forecastStartDate).getMonth() + 1) + '-01 00:00:00');
+    //         d1.setMonth(d1.getMonth() - 1);
+
+    //         this.setState(
+    //             {
+    //                 datasetId: pID,
+    //                 rangeValue: { from: { year: startDateSplit[1] - 3, month: new Date(selectedForecastProgram.forecastStartDate).getMonth() + 1 }, to: { year: forecastStopDate.getFullYear(), month: forecastStopDate.getMonth() + 1 } },
+    //                 startDateDisplay: months[new Date(selectedForecastProgram.forecastStartDate).getMonth()] + ' ' + (startDateSplit[1] - 3),
+    //                 endDateDisplay: months[(forecastStopDate.getMonth())] + ' ' + forecastStopDate.getFullYear(),
+    //                 beforeEndDateDisplay: months[(d1.getMonth())] + ' ' + d1.getFullYear(),
+    //             }, () => {
+    //                 // console.log("d----------->0", d1);
+    //                 // console.log("d----------->00", (d1.getMonth()));
+    //                 // console.log("d----------->1", this.state.startDateDisplay);
+    //                 // console.log("d----------->2", this.state.endDateDisplay);
+    //                 // console.log("d----------->3", this.state.beforeEndDateDisplay);
+
+    //                 this.filterData();
+    //             })
+    //     } else {
+    //         var dt = new Date();
+    //         dt.setMonth(dt.getMonth() - REPORT_DATEPICKER_START_MONTH);
+    //         var dt1 = new Date();
+    //         dt1.setMonth(dt1.getMonth() + REPORT_DATEPICKER_END_MONTH);
+    //         this.setState(
+    //             {
+    //                 datasetId: 0,
+    //                 rangeValue: { from: { year: dt.getFullYear(), month: dt.getMonth() + 1 }, to: { year: dt1.getFullYear(), month: dt1.getMonth() + 1 } },
+    //             }, () => {
+    //                 this.el = jexcel(document.getElementById("tableDiv"), '');
+    //                 this.el.destroy();
+    //                 this.filterData();
+    //             })
+    //     }
+
+    // }
+
     filterData() {
 
         let startDate = this.state.rangeValue.from.year + '-' + this.state.rangeValue.from.month + '-01';
@@ -984,52 +1034,6 @@ export default class PlanningUnitSetting extends Component {
                     forecastProgramVersionId: forecastProgramVersionId,
                     selsource: selectedForecastProgram.planningUnitList,
                     loading: true,
-                    // selsource: [
-                    //     {
-                    //         "a1a": "ARVs",
-                    //         "a2a": "Abacavir 300 mg Tablet, 60 Tablets",
-                    //         "a3a": true,
-                    //         "a4a": true,
-                    //         "a5a": "11,199",
-                    //         "a6a": "54,714",
-                    //         "a7a": "5",
-                    //         "a8a": "Custom",
-                    //         "a9a": "1.06"
-                    //     },
-                    //     {
-                    //         "a1a": "ARVs",
-                    //         "a2a": "Lamivudine 10 mg/mL Solution, 100 mL",
-                    //         "a3a": true,
-                    //         "a4a": true,
-                    //         "a5a": "10,938",
-                    //         "a6a": "51,751",
-                    //         "a7a": "5",
-                    //         "a8a": "GHSC-PSM",
-                    //         "a9a": "3.06"
-                    //     },
-                    //     {
-                    //         "a1a": "Condoms",
-                    //         "a2a": "Male Condom (Latex) Lubricated, No Logo, 49 mm, 1 Each",
-                    //         "a3a": true,
-                    //         "a4a": false,
-                    //         "a5a": "19,352",
-                    //         "a6a": "84,472",
-                    //         "a7a": "5",
-                    //         "a8a": "GHSC-PSM",
-                    //         "a9a": "4.10"
-                    //     },
-                    //     {
-                    //         "a1a": "Condoms",
-                    //         "a2a": "Male Condom (Latex) Lubricated, Ultimate Blue, 53 mm, 4320 Pieces",
-                    //         "a3a": true,
-                    //         "a4a": true,
-                    //         "a5a": "12,633",
-                    //         "a6a": "83,678",
-                    //         "a7a": "5",
-                    //         "a8a": "UNFPA",
-                    //         "a9a": "5.10"
-                    //     }
-                    // ],
                     selectedForecastProgram: selectedForecastProgram,
                 }, () => {
                     // this.buildJExcel();
