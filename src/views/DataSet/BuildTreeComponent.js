f2c7748b6e551348596a1dc19284d52dbf547889
Anchal Chauhan <anchal.c@altius.cc>
2022-01-19 16:04:02 +0530

    Committed by  anchal

--- src/views/DataSet/BuildTreeComponent.js
+++ src/views/DataSet/BuildTreeComponent.js
@@ -1475,7 +1475,7 @@ export default class BuildTree extends Component {
             data[0] = momList[j].month
             data[1] = parseFloat(momList[j].startValue).toFixed(2)
             data[2] = parseFloat(momList[j].difference).toFixed(2)
-            data[3] = parseFloat(momList[j].endValueWMC).toFixed(2)
+            data[3] = parseFloat(momList[j].startValue - momList[j].difference).toFixed(2)
             data[4] = parseFloat(momList[j].seasonalityPerc).toFixed(2)
             data[5] = parseFloat(momList[j].manualChange).toFixed(2)
             data[6] = parseFloat(momList[j].endValue).toFixed(2)
