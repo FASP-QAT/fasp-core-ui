49ea449045334058c68a5a04afde500e9ec24b2d
Anisha Selote <Anisha.s@altius.cc>
2022-03-03 17:49:04 +0530

    commited by anisha

--- src/views/DataSet/VersionSettingsComponent.js
+++ src/views/DataSet/VersionSettingsComponent.js
@@ -1405,7 +1405,7 @@ class VersionSettingsComponent extends Component {
                     </CardFooter>
                 </Card>
                 <Modal isOpen={this.state.showValidation}
-                    className={'modal-lg ' + this.props.className} id='divcontents'>
+                    className={ 'modal-fixed-footer', 'modal-lg ' + this.props.className} id='divcontents'>
                     {/* <ModalHeader toggle={() => this.toggleShowValidation()} className="modalHeaderSupplyPlan">
                         <h3 style={{textAlign:'left'}}><strong>{i18n.t('static.commitTree.forecastValidation')}</strong><i className="fa fa-print pull-right iconClass cursor" onClick={() => this.print()}></i></h3>
                     </ModalHeader> */}
@@ -1417,7 +1417,7 @@ class VersionSettingsComponent extends Component {
                         </div>
                     </ModalHeader>
                     <div>
-                        <ModalBody>
+                        <ModalBody className="VersionSettingMode">
                             <span><b>{this.state.programName}</b></span><br />
                             <span><b>{i18n.t('static.common.forecastPeriod')}: </b> {moment(this.state.forecastStartDate).format('MMM-YYYY')} to {moment(this.state.forecastStopDate).format('MMM-YYYY')} </span><br /><br />
 
@@ -1490,11 +1490,15 @@ class VersionSettingsComponent extends Component {
                                     </Table>
                                 </div> : <span>{treeNodes}</span>}
                             </div>
-                            <div className="col-md-12 pb-lg-5 pt-lg-3">
+                            {/* <div className="col-md-12 pb-lg-5 pt-lg-3">
+                                <Button type="button" size="md" color="danger" className="float-right mr-1" onClick={() => { this.openModalPopup() }}><i className="fa fa-times"></i> {i18n.t('static.common.cancel')}</Button>
+                                <Button type="submit" color="success" className="mr-1 float-right" size="md" onClick={this.synchronize}><i className="fa fa-check"></i>{i18n.t('static.report.ok')}</Button>
+                            </div> */}
+                        </ModalBody>
+                        <div className="col-md-12 pb-lg-5 pt-lg-3">
                                 <Button type="button" size="md" color="danger" className="float-right mr-1" onClick={() => { this.openModalPopup() }}><i className="fa fa-times"></i> {i18n.t('static.common.cancel')}</Button>
                                 <Button type="submit" color="success" className="mr-1 float-right" size="md" onClick={this.synchronize}><i className="fa fa-check"></i>{i18n.t('static.report.ok')}</Button>
                             </div>
-                        </ModalBody>
                     </div>
                 </Modal >
             </div>
