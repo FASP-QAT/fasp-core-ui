9bb0cabef614f76d38041a02a9c9b18afec188f0
Anchal Chauhan <anchal.c@altius.cc>
2022-04-01 20:28:51 +0530

    Committed by anchal

--- src/views/DataSet/CreateTreeTemplateComponent.js
+++ src/views/DataSet/CreateTreeTemplateComponent.js
@@ -362,33 +362,6 @@ function addCommas(cell1, row) {
     }
 }
 
-function addCommasAllowTenDigitBeforeDecimal(cell1, row) {
-
-    if (cell1 != null && cell1 != "") {
-        console.log("Comma---Inside if");
-        cell1 += '';
-        console.log("Comma---append blank");
-        var x = cell1.replaceAll(",", "").split('.');
-        console.log("Comma---x---", x);
-        var x1 = x[0];
-        console.log("Comma---x1---", x1);
-        var x2 = x.length > 1 ? '.' + x[1].slice(0, 4) : '';
-        console.log("Comma---x2---", x2);
-        var rgx = /(\d+)(\d{10})/;
-        console.log("Comma---reg");
-        while (rgx.test(x1)) {
-            console.log("Comma---indide while");
-            x1 = x1.replace(rgx, '$1' + ',' + '$2');
-            console.log("Comma---x1 replace---", x1);
-        }
-        console.log("Comma---x1+x2---", x1 + x2);
-        return x1 + x2;
-        // return cell1.toString().replaceAll(",", "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-    } else {
-        console.log("Comma---");
-        return "";
-    }
-}
 
 function addCommasTwoDecimal(cell1, row) {
     if (cell1 != null && cell1 != "") {
@@ -6394,7 +6367,7 @@ export default class CreateTreeTemplate extends Component {
 
                                             </FormGroup>
                                             <FormGroup className="col-md-5" style={{ display: this.state.currentItemConfig.context.payload.nodeType.id == 4 ? 'block' : 'none' }} >
-                                                <Input type="number"
+                                                <Input type="text"
                                                     id="noOfPersons"
                                                     name="noOfPersons"
                                                     bsSize="sm"
@@ -6406,7 +6379,7 @@ export default class CreateTreeTemplate extends Component {
                                                         handleChange(e);
                                                         this.dataChange(e)
                                                     }}
-                                                    value={addCommasAllowTenDigitBeforeDecimal(this.state.currentItemConfig.context.payload.nodeType.id == 4 ? this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.noOfPersons : "")}>
+                                                    value={addCommas(this.state.currentItemConfig.context.payload.nodeType.id == 4 ? this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.noOfPersons : "")}>
 
                                                 </Input>
                                                 <FormFeedback className="red">{errors.noOfPersons}</FormFeedback>
@@ -6434,7 +6407,7 @@ export default class CreateTreeTemplate extends Component {
                                                 <Label htmlFor="currencyId">{i18n.t('static.tree.requires')}<span class="red Reqasterisk">*</span></Label>
                                             </FormGroup>
                                             <FormGroup className="col-md-5" style={{ display: this.state.currentItemConfig.context.payload.nodeType.id == 4 ? 'block' : 'none' }}>
-                                                <Input type="number"
+                                                <Input type="text"
                                                     id="forecastingUnitPerPersonsFC"
                                                     name="forecastingUnitPerPersonsFC"
                                                     bsSize="sm"
@@ -6445,7 +6418,7 @@ export default class CreateTreeTemplate extends Component {
                                                         handleChange(e);
                                                         this.dataChange(e)
                                                     }}
-                                                    value={addCommasAllowTenDigitBeforeDecimal(this.state.currentItemConfig.context.payload.nodeType.id == 4 ? this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.noOfForecastingUnitsPerPerson : "")}>
+                                                    value={addCommas(this.state.currentItemConfig.context.payload.nodeType.id == 4 ? this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.noOfForecastingUnitsPerPerson : "")}>
 
                                                 </Input>
                                                 <FormFeedback className="red">{errors.forecastingUnitPerPersonsFC}</FormFeedback>
@@ -6510,7 +6483,7 @@ export default class CreateTreeTemplate extends Component {
                                                             name="usageFrequency"
                                                             bsSize="sm"
                                                             onChange={(e) => { this.dataChange(e) }}
-                                                            value={this.state.currentItemConfig.context.payload.nodeType.id == 4 && this.state.currentItemConfig.context.payload.nodeDataMap != "" && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.usageType.id == 1 && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.oneTimeUsage != "true" ? addCommasAllowTenDigitBeforeDecimal(this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.usageFrequency) : ""}></Input>
+                                                            value={this.state.currentItemConfig.context.payload.nodeType.id == 4 && this.state.currentItemConfig.context.payload.nodeDataMap != "" && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.usageType.id == 1 && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.oneTimeUsage != "true" ? addCommas(this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.usageFrequency) : ""}></Input>
                                                     </FormGroup>
                                                     <FormGroup className="col-md-2" style={{ display: this.state.currentItemConfig.context.payload.nodeType.id == 4 && this.state.currentItemConfig.context.payload.nodeDataMap != "" && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.usageType.id == 1 && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.oneTimeUsage != "true" && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.oneTimeUsage != true ? 'block' : 'none' }}>
                                                         <Input type="text"
@@ -6544,7 +6517,7 @@ export default class CreateTreeTemplate extends Component {
                                                         <Label htmlFor="currencyId">for<span class="red Reqasterisk">*</span></Label>
                                                     </FormGroup>
                                                     <FormGroup className="col-md-5" style={{ display: this.state.currentItemConfig.context.payload.nodeType.id == 4 && this.state.currentItemConfig.context.payload.nodeDataMap != "" && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.usageType.id == 1 && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.oneTimeUsage != "true" && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.oneTimeUsage != true ? 'block' : 'none' }}>
-                                                        <Input type="number"
+                                                        <Input type="text"
                                                             id="repeatCount"
                                                             name="repeatCount"
                                                             bsSize="sm"
@@ -6555,7 +6528,7 @@ export default class CreateTreeTemplate extends Component {
                                                                 handleChange(e);
                                                                 this.dataChange(e)
                                                             }}
-                                                            value={addCommasAllowTenDigitBeforeDecimal(this.state.currentItemConfig.context.payload.nodeType.id == 4 && this.state.currentItemConfig.context.payload.nodeDataMap != "" && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.usageType.id == 1 && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.oneTimeUsage != "true" ? this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.repeatCount : "")}>
+                                                            value={addCommas(this.state.currentItemConfig.context.payload.nodeType.id == 4 && this.state.currentItemConfig.context.payload.nodeDataMap != "" && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.usageType.id == 1 && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.oneTimeUsage != "true" ? this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.repeatCount : "")}>
                                                         </Input>
                                                         <FormFeedback className="red">{errors.repeatCount}</FormFeedback>
                                                     </FormGroup>
@@ -6592,7 +6565,7 @@ export default class CreateTreeTemplate extends Component {
                                                     <Label htmlFor="currencyId">{i18n.t('static.usageTemplate.every')}<span class="red Reqasterisk">*</span></Label>
                                                 </FormGroup>
                                                 <FormGroup className="col-md-5" style={{ display: this.state.currentItemConfig.context.payload.nodeType.id == 4 && this.state.currentItemConfig.context.payload.nodeDataMap != "" && this.state.currentItemConfig.context.payload.nodeDataMap[0][0].fuNode.usageType.id == 2 ? 'block' : 'none' }}>
-                                                    <Input type="number"
+                                                    <Input type="text"
                                                         id="usageFrequency"
                                                         name="usageFrequency"
                                                         bsSize="sm"
@@ -6852,12 +6825,12 @@ export default class CreateTreeTemplate extends Component {
                                         </div>
                                         <FormGroup className="col-md-5">
                                             <Label htmlFor="currencyId">{i18n.t('static.tree.targetEnding')} {this.state.currentItemConfig.context.payload.nodeType.id == 2 ? 'value' : '%'}<span class="red Reqasterisk">*</span> <i class="fa fa-info-circle icons pl-lg-2" id="Popover22" onClick={this.toggleTargetEndingValue} aria-hidden="true" style={{ color: '#002f6c', cursor: 'pointer' }}></i></Label>
-                                            <Input type="number"
+                                            <Input type="text"
                                                 id="currentEndValue"
                                                 name="currentEndValue"
                                                 bsSize="sm"
                                                 onChange={(e) => { this.dataChange(e); this.calculateMomByEndValue(e) }}
-                                                value={this.state.currentItemConfig.context.payload.nodeType.id == 2 ? addCommasAllowTenDigitBeforeDecimal(this.state.currentEndValue) : addCommas(this.state.currentEndValue)}
+                                                value={addCommas(this.state.currentEndValue)}
                                                 readOnly={this.state.currentEndValueEdit}
                                             >
                                             </Input>
@@ -6904,7 +6877,7 @@ export default class CreateTreeTemplate extends Component {
                                                 name="currentTargetChangeNumber"
                                                 bsSize="sm"
                                                 onChange={(e) => { this.dataChange(e); this.calculateMomByChangeInNumber(e) }}
-                                                value={addCommasAllowTenDigitBeforeDecimal(this.state.currentTargetChangeNumber)}
+                                                value={addCommas(this.state.currentTargetChangeNumber)}
                                                 readOnly={this.state.currentTargetChangeNumberEdit}
                                             >
                                             </Input>
