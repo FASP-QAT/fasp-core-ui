b979e0a1336cc79755958043a7df7123552233ac
shubham deulkar <shubham.d@altius.cc>
2022-04-20 08:49:06 +0530

    commit by shubham bugs

--- src/views/ForecastingReports/ForecastOutput.js
+++ src/views/ForecastingReports/ForecastOutput.js
@@ -1699,9 +1699,9 @@ class ForecastOutput extends Component {
                                 monthArrayList: years,
                                 message: ''
                             }, () => {
-                                // if (yaxisEquUnitId > 0) {
-                                //     this.calculateEquivalencyUnitTotal();
-                                // }
+                                if (yaxisEquUnitId > 0) {
+                                    this.calculateEquivalencyUnitTotal();
+                                }
                             })
 
                         } else {//no
@@ -1710,9 +1710,9 @@ class ForecastOutput extends Component {
                                 monthArrayList: monthArrayList,
                                 message: ''
                             }, () => {
-                                // if (yaxisEquUnitId > 0) {
-                                //     this.calculateEquivalencyUnitTotal();
-                                // }
+                                if (yaxisEquUnitId > 0) {
+                                    this.calculateEquivalencyUnitTotal();
+                                }
                             })
                         }
 
@@ -1756,12 +1756,6 @@ class ForecastOutput extends Component {
                             }
                         }
                     );
-
-
-
-
-
-
 
 
 
@@ -2209,6 +2203,7 @@ class ForecastOutput extends Component {
                                 ForecastingUnitService.getForecastingUnitListByProgramVersionIdForSelectedForecastMap(programId, versionId).then(response => {
                                     console.log('**' + JSON.stringify(response.data))
                                     var listArray = response.data;
+                                    listArray = listArray.filter((v,i,a)=>a.findIndex(v2=>(v2.id===v.id))===i)
                                     listArray.sort((a, b) => {
                                         var itemLabelA = getLabelText(a.label, this.state.lang).toUpperCase(); // ignore upper and lowercase
                                         var itemLabelB = getLabelText(b.label, this.state.lang).toUpperCase(); // ignore upper and lowercase                   
