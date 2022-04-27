2a7c8d06b408cd65a1d3b23cc37d919e31e4670f
dolly chhabriya <dolly.c@altius.cc>
2022-04-27 21:30:22 +0530

    Right click option for modules

--- src/containers/DefaultLayout/DefaultLayout.js
+++ src/containers/DefaultLayout/DefaultLayout.js
@@ -4096,6 +4096,7 @@ class DefaultLayout extends Component {
                       className="bgColourRemoveLink tab1"
                       active={this.state.activeTab === '1'}
                       onClick={() => { this.toggle(0, '1'); }}
+                      href={`/#/ApplicationDashboard/${AuthenticationService.displayDashboardBasedOnRole()}`}
                       style={{ border: "none" }}
                       title={i18n.t('static.module.forecasting')}
                     >
@@ -4108,7 +4109,7 @@ class DefaultLayout extends Component {
                       className="bgColourRemoveLink tab2"
                       active={this.state.activeTab === '2'}
                       onClick={() => { this.toggle(0, '2'); }}
-                      // href={`/#/ApplicationDashboard/${AuthenticationService.displayDashboardBasedOnRole()}`}
+                      href={`/#/ApplicationDashboard/${AuthenticationService.displayDashboardBasedOnRole()}`}
                       style={{ border: "none", padding: "0.75rem 0.2rem" }}
                       title={i18n.t('static.module.supplyPlanningMod')}
                     >
