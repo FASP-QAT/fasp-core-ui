77409dda66192335a7df77f4ab0470407b04eefa
dolly chhabriya <dolly.c@altius.cc>
2022-04-29 14:19:17 +0530

    Inventory turns limiting decimals

--- src/views/Report/InventoryTurns.js
+++ src/views/Report/InventoryTurns.js
@@ -631,7 +631,7 @@ export default class InventoryTurns extends Component {
             data = [];
             data[0] = getLabelText(costOfInventory[j].planningUnit.label, this.state.lang);
             data[1] = (costOfInventory[j].totalConsumption);
-            data[2] = (costOfInventory[j].avergeStock);
+            data[2] = Number(costOfInventory[j].avergeStock).toFixed(2);
             data[3] = (costOfInventory[j].noOfMonths);
             data[4] = (costOfInventory[j].inventoryTurns);
 
