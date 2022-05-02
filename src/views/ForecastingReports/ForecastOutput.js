e3ef2c0474465312ca8796f61bb18a04ad85caba
shubham deulkar <shubham.d@altius.cc>
2022-05-02 09:53:12 +0530

    commit by shubham bugs

--- src/views/ForecastingReports/ForecastOutput.js
+++ src/views/ForecastingReports/ForecastOutput.js
@@ -2247,6 +2247,7 @@ class ForecastOutput extends Component {
                         PlanningUnitService.getPlanningUnitListByProgramVersionIdForSelectedForecastMap(programId, versionId).then(response => {
                             console.log('**' + JSON.stringify(response.data))
                             var listArray = response.data;
+                            listArray = listArray.map(c => c.planningUnit);
                             listArray.sort((a, b) => {
                                 var itemLabelA = getLabelText(a.label, this.state.lang).toUpperCase(); // ignore upper and lowercase
                                 var itemLabelB = getLabelText(b.label, this.state.lang).toUpperCase(); // ignore upper and lowercase                   
