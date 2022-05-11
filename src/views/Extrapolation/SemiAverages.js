3b951b08e1edac6ae786ce91d5cefd5b131766e8
Anchal Chauhan <anchal.c@altius.cc>
2022-05-11 11:10:51 +0530

    Committed by anchal

--- src/views/Extrapolation/SemiAverages.js
+++ src/views/Extrapolation/SemiAverages.js
@@ -1,92 +1,35 @@
-import { calculateError } from '../Extrapolation/ErrorCalculations.js';
-let m = 0;
-let c = 0;
-export function calculateSemiAverages(inputData, noOfProjectionMonths, props) {
-    const data = inputData;
-    console.log("InputData@@@",inputData)
-    console.log("data@@@",data)
-    console.log("result@@@",inputData.length % 2)
-    if (inputData.length % 2 == 1) {
-        inputData.pop();
-        noOfProjectionMonths += 1
-    }
-    const noOfMonthsForProjection = noOfProjectionMonths;
-    let actualMonths = data[data.length - 1].month;
-    initializeSemiAverage(data, actualMonths);
-    for (let x = 1; x <= actualMonths + noOfMonthsForProjection; x++) {
-        if (x <= actualMonths) {
-            var semiAvg = getSemiAverage(x);
-            data[x - 1].forecast = semiAvg > 0 ? semiAvg :0;
-            // data[x - 1].forecast = getSemiAverage(x);
-        } else {
-            var semiAvg = getSemiAverage(x);
-            data[x - 1] = { "month": x, "actual": null, "forecast": semiAvg > 0 ? semiAvg :0};
-            // data[x - 1] = { "month": x, "actual": null, "forecast": getSemiAverage(x) };
-        }
-    }
-    for (let y = 1; y <= actualMonths + noOfMonthsForProjection; y++) {
-        console.log(y + " = " + data[y - 1].forecast);
-    }
-    console.log("going to calculate error semi avaraes---",data)
-    calculateError(data, "semiAvgError", props);
-    console.log("InputData@@@ output tes",data)
-    props.updateState("semiAvgData", data);
-    // Print the output
-
-
-
-}
-
-function getSemiAverage(month) {
-    return m * month + c;
-}
-
-function initializeSemiAverage(data, actualMonths) {
-    let x1 = 0, x2 = 0, y1 = 0, y2 = 0;
-    let cnt = 0;
-    for (let x = 1; x <= actualMonths / 2; x++) {
-        x1 += data[x - 1].month;
-        y1 += data[x - 1].actual;
-        cnt++;
-    }
-    x1 = x1 / cnt;
-    y1 = y1 / cnt;
-    for (let x = actualMonths / 2 + 1; x <= actualMonths; x++) {
-        x2 += data[x - 1].month;
-        y2 += data[x - 1].actual;
-    }
-    x2 = x2 / cnt;
-    y2 = y2 / cnt;
-    m = (y2 - y1) / (x2 - x1);
-    c = m * (0 - x2) + y2;
-}
-
-
 // import { calculateError } from '../Extrapolation/ErrorCalculations.js';
 // let m = 0;
 // let c = 0;
 // export function calculateSemiAverages(inputData, noOfProjectionMonths, props) {
 //     const data = inputData;
+//     console.log("InputData@@@",inputData)
+//     console.log("data@@@",data)
+//     console.log("result@@@",inputData.length % 2)
+//     if (inputData.length % 2 == 1) {
+//         inputData.pop();
+//         noOfProjectionMonths += 1
+//     }
 //     const noOfMonthsForProjection = noOfProjectionMonths;
 //     let actualMonths = data[data.length - 1].month;
 //     initializeSemiAverage(data, actualMonths);
 //     for (let x = 1; x <= actualMonths + noOfMonthsForProjection; x++) {
 //         if (x <= actualMonths) {
 //             var semiAvg = getSemiAverage(x);
-//             data[x - 1].forecast = semiAvg > 0 ? semiAvg : 0;
+//             data[x - 1].forecast = semiAvg > 0 ? semiAvg :0;
 //             // data[x - 1].forecast = getSemiAverage(x);
 //         } else {
 //             var semiAvg = getSemiAverage(x);
-//             data[x - 1] = { "month": x, "actual": null, "forecast": semiAvg > 0 ? semiAvg : 0 };
+//             data[x - 1] = { "month": x, "actual": null, "forecast": semiAvg > 0 ? semiAvg :0};
 //             // data[x - 1] = { "month": x, "actual": null, "forecast": getSemiAverage(x) };
 //         }
 //     }
 //     for (let y = 1; y <= actualMonths + noOfMonthsForProjection; y++) {
 //         console.log(y + " = " + data[y - 1].forecast);
 //     }
-//     console.log("going to calculate error semi avaraes---", data)
+//     console.log("going to calculate error semi avaraes---",data)
 //     calculateError(data, "semiAvgError", props);
-//     console.log("InputData@@@ output tes", data)
+//     console.log("InputData@@@ output tes",data)
 //     props.updateState("semiAvgData", data);
 //     // Print the output
 
@@ -101,16 +44,14 @@ function initializeSemiAverage(data, actualMonths) {
 // function initializeSemiAverage(data, actualMonths) {
 //     let x1 = 0, x2 = 0, y1 = 0, y2 = 0;
 //     let cnt = 0;
-//     for (let x = 1; x <= Math.floor(actualMonths / 2); x++) {
+//     for (let x = 1; x <= actualMonths / 2; x++) {
 //         x1 += data[x - 1].month;
 //         y1 += data[x - 1].actual;
 //         cnt++;
 //     }
 //     x1 = x1 / cnt;
 //     y1 = y1 / cnt;
-//     cnt = 0;
-//     for (let x = Math.floor(actualMonths / 2) + 1; x <= actualMonths; x++) {
-
+//     for (let x = actualMonths / 2 + 1; x <= actualMonths; x++) {
 //         x2 += data[x - 1].month;
 //         y2 += data[x - 1].actual;
 //     }
@@ -119,3 +60,80 @@ function initializeSemiAverage(data, actualMonths) {
 //     m = (y2 - y1) / (x2 - x1);
 //     c = m * (0 - x2) + y2;
 // }
+
+
+import { calculateError } from '../Extrapolation/ErrorCalculations.js';
+let m = 0;
+let c = 0;
+export function calculateSemiAverages(inputData, noOfProjectionMonths, props) {
+    const data = inputData;
+    console.log("@@@inputData---", inputData);
+    const noOfMonthsForProjection = noOfProjectionMonths;
+    console.log("data.lenth 1---", data.length)
+    console.log("data.lenth 2---", data.length - 1)
+    console.log("data.lenth 3---", data[data.length - 1].month)
+
+    let actualMonths = 0;
+    for (let x = 0; x < data.length; x++) {
+        if (data[x].actual != null) {
+            actualMonths++;
+        }
+    }
+    console.log("actualMonths---",actualMonths);
+    initializeSemiAverage(data, actualMonths);
+    console.log("m---", m + " c---", c)
+    for (let x = 1; x <= actualMonths + noOfMonthsForProjection; x++) {
+        if (x <= actualMonths) {
+            var semiAvg = getSemiAverage(x);
+            data[x - 1].forecast = semiAvg > 0 ? semiAvg : 0;
+            // data[x - 1].forecast = getSemiAverage(x);
+        } else {
+            var semiAvg = getSemiAverage(x);
+            data[x - 1] = { "month": x, "actual": null, "forecast": semiAvg > 0 ? semiAvg : 0 };
+            // data[x - 1] = { "month": x, "actual": null, "forecast": getSemiAverage(x) };
+        }
+    }
+    for (let y = 1; y <= actualMonths + noOfMonthsForProjection; y++) {
+        console.log(y + " = " + data[y - 1].forecast);
+    }
+    console.log("going to calculate error semi avaraes---", data)
+    calculateError(data, "semiAvgError", props);
+    console.log("InputData@@@ output tes", data)
+    props.updateState("semiAvgData", data);
+    // Print the output
+
+
+
+}
+
+function getSemiAverage(month) {
+    return m * month + c;
+}
+
+function initializeSemiAverage(data, actualMonths) {
+    let x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+    let cnt = 0;
+    for (let x = 1; x <= Math.floor(actualMonths / 2); x++) {
+        x1 += data[x - 1].month;
+        y1 += data[x - 1].actual;
+        cnt++;
+    }
+    x1 = x1 / cnt;
+    y1 = y1 / cnt;
+    cnt = 0;
+    for (let x = Math.floor(actualMonths / 2) + 1; x <= actualMonths; x++) {
+
+        x2 += data[x - 1].month;
+        y2 += data[x - 1].actual;
+        cnt++;
+    }
+
+    x2 = x2 / cnt;
+    y2 = y2 / cnt;
+    m = (y2 - y1) / (x2 - x1);
+    c = m * (0 - x2) + y2;
+    console.log("x1---",x1);
+    console.log("x2---",x2);
+    console.log("y1---",y1);
+    console.log("y2---",y2);
+}
