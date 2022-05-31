8c21e3153ceae23f63a22de217467bda9cd436db
dolly chhabriya <dolly.c@altius.cc>
2022-05-31 17:59:56 +0530

    Changes in on paste function for ===

--- src/views/EquivalancyUnit/EquivalancyUnitList.js
+++ src/views/EquivalancyUnit/EquivalancyUnitList.js
@@ -130,7 +130,7 @@ class EquivalancyUnit extends Component {
         for (var i = 0; i < data.length; i++) {
             if (z != data[i].y) {
                 var index = (instance.jexcel).getValue(`G${parseInt(data[i].y) + 1}`, true);
-                if (index == "" || index == null || index == undefined) {
+                if (index === "" || index == null || index == undefined) {
                     (instance.jexcel).setValueFromCoords(0, data[i].y, 0, true);
                     (instance.jexcel).setValueFromCoords(8, data[i].y, 1, true);
                     (instance.jexcel).setValueFromCoords(9, data[i].y, 1, true);
@@ -1883,7 +1883,7 @@ class EquivalancyUnit extends Component {
         for (var i = 0; i < data.length; i++) {
             if (z != data[i].y) {
                 var index = (instance.jexcel).getValue(`G${parseInt(data[i].y) + 1}`, true);
-                if (index == "" || index == null || index == undefined) {
+                if (index === "" || index == null || index == undefined) {
                     (instance.jexcel).setValueFromCoords(0, data[i].y, 0, true);
                     (instance.jexcel).setValueFromCoords(8, data[i].y, true, true);
                     (instance.jexcel).setValueFromCoords(11, data[i].y, 1, true);
