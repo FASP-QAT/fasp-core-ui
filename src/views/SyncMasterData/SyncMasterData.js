9884f2073cbe6d3d85b59427fed9864d3511934f
dolly chhabriya <dolly.c@altius.cc>
2022-11-17 15:12:31 +0530

    Bug fixed

--- src/views/SyncMasterData/SyncMasterData.js
+++ src/views/SyncMasterData/SyncMasterData.js
@@ -1139,7 +1139,7 @@ export default class SyncMasterData extends Component {
                                         var datasetDataServerList = ddatasetDataServerRequest.result;
                                         console.log("###datasetDataServerList+++", datasetDataServerList)
                                         var datasetDataServerListFiltered = [];
-                                        if (this.props.location.state != undefined) {
+                                        if (this.props.location.state != undefined && this.props.location.state.programIds!=undefined) {
                                             datasetDataServerListFiltered = datasetDataServerList.filter(c => (this.props.location.state.programIds).includes(c.id));
                                         }
                                         datasetDataServerList.filter(c => c.userId == userId).map(program => {
