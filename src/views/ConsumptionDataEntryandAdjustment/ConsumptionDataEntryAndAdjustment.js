8b479bf63f0093188503663eff6e55f010f98d21
Sonal Kothari <sonal.k@altius.cc>
2022-12-09 17:03:15 +0530

    active check added in DE&A and Extrapolation screen

--- src/views/ConsumptionDataEntryandAdjustment/ConsumptionDataEntryAndAdjustment.js
+++ src/views/ConsumptionDataEntryandAdjustment/ConsumptionDataEntryAndAdjustment.js
@@ -541,7 +541,7 @@ export default class ConsumptionDataEntryandAdjustment extends React.Component {
       }
       this.setState({
         count: count,
-        
+
       })
     }
   }
@@ -938,7 +938,7 @@ export default class ConsumptionDataEntryandAdjustment extends React.Component {
           console.log('consumptionExtrapolationRegression', consumptionExtrapolationRegression);
           datasetJson.consumptionExtrapolation = consumptionExtrapolationList;
           console.log("consumptionExtrapolationList@@@@@@@@@@", consumptionExtrapolationList)
-          console.log("Test DatasetJson@@",datasetJson)
+          console.log("Test DatasetJson@@", datasetJson)
           datasetData = (CryptoJS.AES.encrypt(JSON.stringify(datasetJson), SECRET_KEY)).toString()
           myResult.programData = datasetData;
           var putRequest = datasetTransaction.put(myResult);
@@ -960,12 +960,12 @@ export default class ConsumptionDataEntryandAdjustment extends React.Component {
               countRecived: 0,
               count: 0,
               showDetailTable: true,
-              jsonDataMovingAvg:[],
-              jsonDataSemiAverage:[],
-              jsonDataLinearRegression:[],
-              jsonDataTes:[],
-              jsonDataArima:[],
-              datasetJson:datasetJson
+              jsonDataMovingAvg: [],
+              jsonDataSemiAverage: [],
+              jsonDataLinearRegression: [],
+              jsonDataTes: [],
+              jsonDataArima: [],
+              datasetJson: datasetJson
             }, () => {
               this.hideFirstComponent();
               this.componentDidMount()
@@ -3279,7 +3279,7 @@ export default class ConsumptionDataEntryandAdjustment extends React.Component {
     var stopDate = moment(Date.now()).format("YYYY-MM-DD");
 
     var consumptionList = datasetJson.actualConsumptionList;
-    var datasetPlanningUnit = datasetJson.planningUnitList.filter(c => c.consuptionForecast);
+    var datasetPlanningUnit = datasetJson.planningUnitList.filter(c => c.consuptionForecast && c.active);
     var datasetRegionList = datasetJson.regionList;
     var missingMonthList = [];
 
