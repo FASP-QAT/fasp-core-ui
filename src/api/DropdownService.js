1b4dfe0b9ac146986ee37a16138bc26ce71aa64c
Sonal Kothari <sonal.k@altius.cc>
2023-06-27 19:39:10 +0530

    setup program issues fixed

--- src/api/DropdownService.js
+++ src/api/DropdownService.js
@@ -197,6 +197,7 @@ class DropdownService {
       {}
     );
   }
+  
   getOrganisationListByRealmCountryId(realmCountryId) {
     return axios.get(
       `${API_URL}/api/dropdown/organisation/realmCountryId/${realmCountryId}`,
