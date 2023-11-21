e3259023a1c8ea7f23cca766f4c7500d443bfb36
dolly chhabriya <dolly.c@altius.cc>
2023-11-21 17:38:02 +0530

    Changed role length to 25 characters

--- src/views/Role/AddRoleComponent.js
+++ src/views/Role/AddRoleComponent.js
@@ -917,7 +917,7 @@ class AddRoleComponent extends Component {
                                                         onChange={(e) => { handleChange(e); this.dataChange(e) }}
                                                         onBlur={handleBlur}
                                                         required
-                                                        maxLength={30}
+                                                        maxLength={25}
                                                         value={this.Capitalize(this.state.role.label.label_en)}
                                                     /><FormFeedback className="red">{errors.roleName}</FormFeedback>
                                                 </FormGroup>
