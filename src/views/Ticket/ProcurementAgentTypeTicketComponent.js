5d5f287e355d4b2729afa7c8afeba38507b9e8a8
Rohit_Vishwakarma <rohit.v@altius.cc>
2024-01-17 16:19:37 +0530

    Removed validation from submit button

--- src/views/Ticket/ProcurementAgentTypeTicketComponent.js
+++ src/views/Ticket/ProcurementAgentTypeTicketComponent.js
@@ -325,7 +325,7 @@ export default class ProcurementAgentTypeTicketComponent extends Component {
                                     <ModalFooter className="pb-0 pr-0">
                                         <Button type="button" size="md" color="info" className="mr-1 pr-3 pl-3" onClick={this.props.toggleMaster}><i className="fa fa-angle-double-left "></i>  {i18n.t('static.common.back')}</Button>
                                         <Button type="reset" size="md" color="warning" className="mr-1 text-white" onClick={this.resetClicked}><i className="fa fa-refresh"></i> {i18n.t('static.common.reset')}</Button>
-                                        <Button type="submit" size="md" color="success" className="mr-1" disabled={!isValid}><i className="fa fa-check"></i> {i18n.t('static.common.submit')}</Button>
+                                        <Button type="submit" size="md" color="success" className="mr-1"><i className="fa fa-check"></i> {i18n.t('static.common.submit')}</Button>
                                     </ModalFooter>
                                 </Form>
                             )} />
