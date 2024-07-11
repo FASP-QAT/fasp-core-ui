59cf4704a03de8ea36f5b25bb273c6b5c90629c7
Pankaj Fate <pankaj.f@altius.cc>
2024-07-11 12:36:07 +0530

    generic label issue fixed on edit FU

--- src/views/ForecastingUnit/EditForecastingUnitComponent.js
+++ src/views/ForecastingUnit/EditForecastingUnitComponent.js
@@ -520,7 +520,7 @@ export default class EditForecastingUnitComponent extends Component {
                                                         required />
                                                     <FormFeedback className="red">{errors.label}</FormFeedback>
                                                 </FormGroup>
-                                                <FormGroup>response
+                                                <FormGroup>
                                                     <Label for="genericLabel">{i18n.t('static.product.productgenericname')}</Label>
                                                     <Input type="text"
                                                         name="genericLabel"
