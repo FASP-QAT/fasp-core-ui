98e7e3b613d0acc9cfb9a31fbec87fd6f1f7d8a9
dolly chhabriya <dolly.c@altius.cc>
2024-07-19 01:30:50 +0530

    Bug for continuous loader and semi average formula fixed

--- src/views/Report/EditSupplyPlanStatus.js
+++ src/views/Report/EditSupplyPlanStatus.js
@@ -4967,6 +4967,8 @@ class EditSupplyPlanStatus extends Component {
                                 this.setState({
                                     loading:true
                                 })
+                                var validation = this.checkValidation();
+                                if(validation){
                                 ProgramService.getProgramData({ "programId": this.props.match.params.programId, "versionId": this.props.match.params.versionId })
                                     .then(response => {
                                         let temp_version_status = 0;
@@ -5108,6 +5110,14 @@ class EditSupplyPlanStatus extends Component {
                                                     document.getElementById("submitButton").disabled = false;
                                                     alert("To approve a supply plan – Reviewed must all be checked.");
                                                 }
+                                            }else{
+                                                this.setState({
+                                                    submitMessage: 'static.supplyPlan.validationFailed',
+                                                    submitColor: "red",
+                                                    loading: false
+                                                }, () => {
+                                                    this.hideMessageComponent()
+                                                });
                                             }
                                         } else {
                                             confirmAlert({
@@ -5167,6 +5177,15 @@ class EditSupplyPlanStatus extends Component {
                                             }
                                         }
                                     );
+                                }else{
+                                    this.setState({
+                                        submitMessage: 'static.supplyPlan.validationFailed',
+                                        submitColor: "red",
+                                        loading: false
+                                    }, () => {
+                                        this.hideMessageComponent()
+                                    });
+                                }
                             }}
                             render={
                                 ({
