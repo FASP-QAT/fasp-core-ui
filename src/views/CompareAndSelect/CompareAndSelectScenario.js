8c4069b3d24abfc89ff3e24ec04b9744773aefec
Sonal Kothari <sonal.k@altius.cc>
2024-07-19 18:10:51 +0530

    date issue fixed

--- src/views/CompareAndSelect/CompareAndSelectScenario.js
+++ src/views/CompareAndSelect/CompareAndSelectScenario.js
@@ -93,7 +93,8 @@ class CompareAndSelectScenario extends Component {
             actualConsumptionListForMonth: [],
             changed: false,
             dataChangedFlag: 0,
-            showFits: false
+            showFits: false,
+            actualMinDate: ""
         };
         this.getDatasets = this.getDatasets.bind(this);
         this.setViewById = this.setViewById.bind(this);
@@ -142,8 +143,11 @@ class CompareAndSelectScenario extends Component {
                 this.setMonth1List()
             })
         } else if (e.target.name == "showFits") {
+            var stopDate = this.state.singleValue2.to.year + '-' + this.state.singleValue2.to.month + '-' + new Date(this.state.singleValue2.to.year, this.state.singleValue2.to.month, 0).getDate();
             this.setState({
                 showFits: checked,
+                minDate: { year: Number(moment(this.state.actualMinDate).startOf('month').format("YYYY")), month: Number(moment(this.state.actualMinDate).startOf('month').format("M")) },
+                singleValue2: { from: { year: Number(moment(this.state.actualMinDate).startOf('month').format("YYYY")), month: Number(moment(this.state.actualMinDate).startOf('month').format("M")) }, to: { year: Number(moment(stopDate).startOf('month').format("YYYY")), month: Number(moment(stopDate).startOf('month').format("M")) } },
                 showForecastPeriod: checked ? false : this.state.showForecastPeriod
             }, () => {
                 this.setMonth1List()
@@ -284,6 +288,7 @@ class CompareAndSelectScenario extends Component {
                 forecastNotes: forecastNotes,
                 singleValue2: rangeValue,
                 minDate: { year: Number(moment(actualMinDate).startOf('month').format("YYYY")), month: Number(moment(actualMinDate).startOf('month').format("M")) },
+                actualMinDate: actualMinDate,
                 showAllData: true,
                 loading: false
             }, () => {
