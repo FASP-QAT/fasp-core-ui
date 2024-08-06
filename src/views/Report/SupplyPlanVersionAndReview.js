2fb3d714789cad0655538db52c12ae2ab3180999
dolly chhabriya <dolly.c@altius.cc>
2024-08-06 21:32:18 +0530

    Bugs fixed

--- src/views/Report/SupplyPlanVersionAndReview.js
+++ src/views/Report/SupplyPlanVersionAndReview.js
@@ -302,7 +302,7 @@ class SupplyPlanVersionAndReview extends Component {
             allowInsertColumn: false,
             allowManualInsertColumn: false,
             allowDeleteRow: false,
-            onselection: this.selected,
+            // onselection: this.selected,
             oneditionend: this.onedit,
             copyCompatibility: true,
             allowExport: false,
