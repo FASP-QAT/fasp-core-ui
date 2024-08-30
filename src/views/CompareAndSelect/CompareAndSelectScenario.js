d77ee096dbd8695812f3b5a533a230a7fed2a46b
Sonal Kothari <sonal.k@altius.cc>
2024-08-30 20:05:49 +0530

    fiscal year change fixed

--- src/views/CompareAndSelect/CompareAndSelectScenario.js
+++ src/views/CompareAndSelect/CompareAndSelectScenario.js
@@ -1955,12 +1955,14 @@ class CompareAndSelectScenario extends Component {
                 }
             }
         }
+        console.log("val", val)
         this.setState({
             xAxisDisplayBy: displayBy,
             singleValue2: val,
             loading: false
         }, () => {
-            this.buildJexcel();
+            this.getData();
+            // this.buildJexcel();
             // this.expandCompressFuntion();
         })
     }
