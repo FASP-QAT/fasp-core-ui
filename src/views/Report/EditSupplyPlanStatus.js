6324a790295fbf87f49907cd62199d16284cb3b3
dolly chhabriya <dolly.c@altius.cc>
2024-09-26 10:30:04 +0530

    Bug for edit supply plan status fixed

--- src/views/Report/EditSupplyPlanStatus.js
+++ src/views/Report/EditSupplyPlanStatus.js
@@ -5372,9 +5372,10 @@ observer.observe(document.documentElement, {
                                                     var json = elInstance.getJson();
                                                     var reviewedProblemList = [];
                                                     var isAllCheckForReviewed = true;
+                                                    var j=0;
                                                     for (var i = 0; i < json.length; i++) {
                                                         var map = new Map(Object.entries(json[i]));
-                                                        if ((map.get("0") == "" || map.get("0") == 0) || (map.get("23") == 1 && (this.state.problemList[i].problemStatus.id != map.get("11") || this.state.problemList[i].reviewed.toString() != map.get("21").toString() || map.get("22").toString() != ""))) {
+                                                        if ((map.get("0") == "" || map.get("0") == 0) || (map.get("23") == 1 && (this.state.problemList[j].problemStatus.id != map.get("11") || this.state.problemList[j].reviewed.toString() != map.get("21").toString() || map.get("22").toString() != ""))) {
                                                             reviewedProblemList.push({
                                                                 problemReportId: map.get("0"),
                                                                 problemStatus: {
@@ -5384,6 +5385,9 @@ observer.observe(document.documentElement, {
                                                                 reviewedNotes: map.get("22"),
                                                                 notes: map.get("22")
                                                             });
+                                                        }
+                                                        if((map.get("0") != "" && map.get("0") != 0)){
+                                                            j+=1;
                                                         }
                                                         if (map.get("21") == false && map.get("13") != 4) {
                                                             isAllCheckForReviewed = false
