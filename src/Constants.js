870ec254207446f1a5ce411e2c702c46b8fdb1d4
Sonal Kothari <sonal.k@altius.cc>
2024-10-10 09:51:56 +0530

    url fix

--- src/Constants.js
+++ src/Constants.js
@@ -8,9 +8,6 @@
 export const API_URL = 'https://api.quantificationanalytics.org'
 // export const API_URL = 'https://demo-api.quantificationanalytics.org'
 
-export const API_URL = 'https://uatqatapi.altius.cc'
-
-
 // export const API_URL='http://192.168.43.70:8080/FASP'
 
 export const JIRA_SUBJECT_PREFIX_UAT = 'UAT - '
