668f8632a7938518952c92b30bbbc31d289dcd96
Sonal Kothari <sonal.k@altius.cc>
2024-10-17 09:58:27 +0530

    labels added and version changed

--- src/Constants.js
+++ src/Constants.js
@@ -1,8 +1,8 @@
 // export const API_URL='https://fasp.altius.cc/FASP'
 // export const API_URL='https://api-mod2.altius.cc'
-export const API_URL = 'https://uatqatapi.altius.cc'
+// export const API_URL = 'https://uatqatapi.altius.cc'
 // export const API_URL = 'http://server6.altius.cc:8080/FASP'
-// export const API_URL = 'http://localhost:8084'
+export const API_URL = 'http://localhost:8084'
 
 // export const API_URL = 'https://uat-api.quantificationanalytics.org'
 // export const API_URL = 'https://api.quantificationanalytics.org'
@@ -65,7 +65,7 @@ export const JIRA_PROJECT_ISSUE_TYPE_ADD_UPDATE_MASTER_DATA = "Add / Update Mast
 export const JEXCEL_DATE_FORMAT = "DD-MON-YY"
 export const JEXCEL_DATE_FORMAT_SM = "DD-Mon-YY"
 export const JEXCEL_DATE_FORMAT_WITHOUT_DATE = "MON-YY"
-export const APP_VERSION_REACT = "[12852]"
+export const APP_VERSION_REACT = "[12882]"
 export const BATCH_PREFIX = "QAT"
 export const NONE_SELECTED_DATA_SOURCE_ID = 17
 export const JEXCEL_PAGINATION_OPTION = [15, 25, 50, 5000000]
