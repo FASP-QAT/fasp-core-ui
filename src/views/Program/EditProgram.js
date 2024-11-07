8db4ec02d58ede51fe2edea5a76480596642a407
dolly chhabriya <dolly.c@altius.cc>
2024-11-07 13:47:56 +0530

    Changes for check user acl

--- src/views/Program/EditProgram.js
+++ src/views/Program/EditProgram.js
@@ -477,7 +477,7 @@ export default class EditProgram extends Component {
                 .then(response => {
                     if (response.status == 200) {
                         var haList = [];
-                        if (AuthenticationService.getLoggedInUserRoleBusinessFunctionArray().includes("ROLE_BF_UPDATE_TA_FOR_SP")) {
+                        if (AuthenticationService.checkUserACL([this.props.match.params.programId.toString()], "ROLE_BF_UPDATE_TA_FOR_SP")) {
                             var json = response.data;
                             for (var i = 0; i < json.length; i++) {
                                 haList[i] = { healthAreaCode: json[i].healthAreaCode, value: json[i].healthAreaId, label: getLabelText(json[i].label, this.state.lang) }
@@ -966,7 +966,7 @@ export default class EditProgram extends Component {
                                                                     type="text"
                                                                     maxLength={6}
                                                                     value={this.state.uniqueCode}
-                                                                    disabled={!AuthenticationService.getLoggedInUserRoleBusinessFunctionArray().includes("ROLE_BF_UPDATE_PC_FOR_SP") ? true : false}
+                                                                    disabled={!AuthenticationService.checkUserACL([this.props.match.params.programId.toString()], "ROLE_BF_UPDATE_PC_FOR_SP") ? true : false}
                                                                     name="programCode1" id="programCode1" />
                                                             </FormGroup>
                                                         </Col>
@@ -1022,7 +1022,7 @@ export default class EditProgram extends Component {
                                                             type="select"
                                                             name="organisationId"
                                                             id="organisationId"
-                                                            disabled={!AuthenticationService.getLoggedInUserRoleBusinessFunctionArray().includes("ROLE_BF_UPDATE_ORG_FOR_SP") ? true : false}
+                                                            disabled={!AuthenticationService.checkUserACL([this.props.match.params.programId.toString()], "ROLE_BF_UPDATE_ORG_FOR_SP") ? true : false}
                                                             value={this.state.program.organisation.id}
                                                             onChange={(e) => { handleChange(e); this.dataChange(e); this.generateOrganisationCode(e) }}
                                                         >
@@ -1070,7 +1070,7 @@ export default class EditProgram extends Component {
                                                             multi
                                                             options={this.state.healthAreaList}
                                                             value={this.state.program.healthAreaArray}
-                                                            disabled={!AuthenticationService.getLoggedInUserRoleBusinessFunctionArray().includes("ROLE_BF_UPDATE_TA_FOR_SP") ? true : false}
+                                                            disabled={!AuthenticationService.checkUserACL([this.props.match.params.programId.toString()], "ROLE_BF_UPDATE_TA_FOR_SP") ? true : false}
                                                             name="healthAreaId"
                                                             id="healthAreaId"
                                                             placeholder={i18n.t('static.common.select')}
