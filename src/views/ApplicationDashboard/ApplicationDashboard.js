b9a23bb1e23316b7e77fdf2596ff87f8d5336de7
Rohit_Vishwakarma <rohit.v@altius.cc>
2024-11-14 23:34:24 +0530

    Fixed bug

--- src/views/ApplicationDashboard/ApplicationDashboard.js
+++ src/views/ApplicationDashboard/ApplicationDashboard.js
@@ -534,11 +534,12 @@ class ApplicationDashboard extends Component {
    */
   checkNewerVersions(programs) {
     if (localStorage.getItem('sessionType') === 'Online') {
-      ProgramService.checkNewerVersions(programs)
-        .then(response => {
-          localStorage.removeItem("sesLatestProgram");
-          localStorage.setItem("sesLatestProgram", response.data);
-        })
+      localStorage.setItem("sesLatestProgram", this.state.dashboardTopList.filter(x => !x.isLatest).length);
+      // ProgramService.checkNewerVersions(programs)
+      //   .then(response => {
+      //     localStorage.removeItem("sesLatestProgram");
+      //     localStorage.setItem("sesLatestProgram", response.data);
+      //   })
     }
   }
   /**
@@ -2967,7 +2968,7 @@ class ApplicationDashboard extends Component {
                     {(this.state.dashboardTopList.length > 0 || this.state.topProgramId.length > 0) && <div class="table-responsive fixTableHeadTopDashboard tableFixHeadDash">
                       <Table className="table-striped table-bordered text-center">
                         <thead>
-                          {localStorage.getItem("topLocalProgram") == "true" && <th scope="col">Delete <i class="fa fa-info-circle icons" title={i18n.t("static.dashboard.actionTooltip")} aria-hidden="true" style={{ color: '#002f6c', cursor: 'pointer' }}></i></th>}
+                          {localStorage.getItem("topLocalProgram") == "true" && <th scope="col">Action <i class="fa fa-info-circle icons" title={i18n.t("static.dashboard.actionTooltip")} aria-hidden="true" style={{ color: '#002f6c', cursor: 'pointer' }}></i></th>}
                           <th scope="col">Program <i class="fa fa-info-circle icons" title={i18n.t("static.dashboard.programTooltip")} aria-hidden="true" style={{ color: '#002f6c', cursor: 'pointer' }}></i></th>
                           <th scope="col" width="125px">Active Planning Units</th>
                           <th scope="col">Planning Units With Stockouts <i class="fa fa-info-circle icons" title={i18n.t("static.dashboard.stockoutTooltip")} aria-hidden="true" style={{ color: '#002f6c', cursor: 'pointer' }}></i></th>
@@ -2981,7 +2982,9 @@ class ApplicationDashboard extends Component {
                             return (
                               <tr>
                                 {localStorage.getItem("topLocalProgram") == "true" && <td scope="row">
-                                  <i class="fa fa-trash" style={{ color: "danger", cursor: "pointer" }} title="Delete" onClick={() => this.deleteSupplyPlanProgram(d.program.id.split("_")[0], d.program.id.split("_")[1].slice(1))}></i> &nbsp;
+                                  <i class="fa fa-trash icons" style={{ color: "danger", cursor: "pointer" }} title="Delete" onClick={() => this.deleteSupplyPlanProgram(d.program.id.split("_")[0], d.program.id.split("_")[1].slice(1))}></i> &nbsp;
+                                  <i class="cui-cloud-download icons" style={{ color: d.isLatest ? "" : "#FF0000", cursor: "pointer" }} title="Download" onClick={() => this.deleteSupplyPlanProgram(d.program.id.split("_")[0], d.program.id.split("_")[1].slice(1))}></i> &nbsp;
+                                  <i class="cui-cloud-upload icons" style={{ color: "danger", cursor: "pointer" }} title="Upload" onClick={() => this.deleteSupplyPlanProgram(d.program.id.split("_")[0], d.program.id.split("_")[1].slice(1))}></i> &nbsp;
                                   {/* <i class="fa fa-refresh" style={{ color: "info", cursor: "pointer" }} title="Re-calculate QPL" onClick={() => this.getProblemListAfterCalculation(d.program.id)}></i> */}
                                 </td>}
                                 {localStorage.getItem("topLocalProgram") == "true" && <td scope="row">{d.program.code + " ~v" + d.program.version} {d.versionType.id == 2 && d.versionStatus.id == 2 ? "*" : ""}​</td>}
--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -66,7 +66,7 @@ export function Dashboard(props, programId, reportBy, updateTopPart, updateBotto
                                         "versionType": programJson.currentVersion.versionType,
                                         "versionStatus": programJson.currentVersion.versionStatus,
                                         "latestFinalVersion":p[0].versionList.filter(c=>c.versionType.id==FINAL_VERSION_TYPE).slice(-1)[0],
-                                        isLatest:p[0].currentVersion.versionId>item.version?false:true
+                                        "isLatest": p[0].currentVersion.versionId > item.version ? false : true
                                     }
                                     dashboradTopList.push(dashboradTop);
                                 }
