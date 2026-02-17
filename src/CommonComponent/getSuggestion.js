eac2239ad71003b5ecc694b39a1a31f17209179d
dolly chhabriya <dolly.c@altius.cc>
2026-02-17 22:34:23 +0530

    Bug fixed

--- src/CommonComponent/getSuggestion.js
+++ src/CommonComponent/getSuggestion.js
@@ -180,16 +180,16 @@ export default function getSuggestion(row, lang) {
         var desc_pr = row.realmProblem.problem.actionLabel.label_pr;
         var label = row.realmProblem.problem.actionLabel;
         if (desc_en != null && desc_en != '') {
-            const result_en = desc_en.split('<%PLANNING_UNIT%>').join(getLabelText(row.planningUnit.label, lang)).split('<%REGION%>').join(getLabelText(row.region.label, lang)).split('<%MONTHARRAY%>').join(JSON.parse(row.data5)).split('<%NOOFMONTHS%>').join((row.data5).split(', ').length);
+            const result_en = desc_en.split('<%PLANNING_UNIT%>').join(getLabelText(row.planningUnit.label, lang)).split('<%REGION%>').join(getLabelText(row.region.label, lang)).split('<%MONTHARRAY%>').join(JSON.parse((row.data5.toString().replaceAll(",",", ")))).split('<%NOOFMONTHS%>').join((row.data5).split(', ').length);
             label.label_en = result_en;
         } if (desc_fr != null && desc_fr != '') {
-            const result_fr = desc_fr.split('<%PLANNING_UNIT%>').join(getLabelText(row.planningUnit.label, lang)).split('<%REGION%>').join(getLabelText(row.region.label, lang)).split('<%MONTHARRAY%>').join(JSON.parse(row.data5)).split('<%NOOFMONTHS%>').join((row.data5).split(', ').length);
+            const result_fr = desc_fr.split('<%PLANNING_UNIT%>').join(getLabelText(row.planningUnit.label, lang)).split('<%REGION%>').join(getLabelText(row.region.label, lang)).split('<%MONTHARRAY%>').join(JSON.parse((row.data5.toString().replaceAll(",",", ")))).split('<%NOOFMONTHS%>').join((row.data5).split(', ').length);
             label.label_fr = result_fr;
         } if (desc_sp != null && desc_sp != '') {
-            const result_sp = desc_sp.split('<%PLANNING_UNIT%>').join(getLabelText(row.planningUnit.label, lang)).split('<%REGION%>').join(getLabelText(row.region.label, lang)).split('<%MONTHARRAY%>').join(JSON.parse(row.data5)).split('<%NOOFMONTHS%>').join((row.data5).split(', ').length);
+            const result_sp = desc_sp.split('<%PLANNING_UNIT%>').join(getLabelText(row.planningUnit.label, lang)).split('<%REGION%>').join(getLabelText(row.region.label, lang)).split('<%MONTHARRAY%>').join(JSON.parse((row.data5.toString().replaceAll(",",", ")))).split('<%NOOFMONTHS%>').join((row.data5).split(', ').length);
             label.label_sp = result_sp;
         } if (desc_pr != null && desc_pr != '') {
-            const result_pr = desc_pr.split('<%PLANNING_UNIT%>').join(getLabelText(row.planningUnit.label, lang)).split('<%REGION%>').join(getLabelText(row.region.label, lang)).split('<%MONTHARRAY%>').join(JSON.parse(row.data5)).split('<%NOOFMONTHS%>').join((row.data5).split(', ').length);
+            const result_pr = desc_pr.split('<%PLANNING_UNIT%>').join(getLabelText(row.planningUnit.label, lang)).split('<%REGION%>').join(getLabelText(row.region.label, lang)).split('<%MONTHARRAY%>').join(JSON.parse((row.data5.toString().replaceAll(",",", ")))).split('<%NOOFMONTHS%>').join((row.data5).split(', ').length);
             label.label_pr = result_pr;
         }
         return getLabelText(label, lang);
