43e18db3a0da86bcf19e05fdc5a83f3bae84b5cf
Rohit Vishwakarma <rohit.v@altius.cc>
2026-04-28 12:11:00 +0530

    Fixed frozen column issue

--- src/views/PlanningUnitSetting/PlanningUnitSetting.js
+++ src/views/PlanningUnitSetting/PlanningUnitSetting.js
@@ -156,7 +156,7 @@ export default class PlanningUnitSetting extends Component {
                 var map = new Map(Object.entries(json[i]));
                 var planningUnitValue = map.get("1");
                 if (planningUnitValue == value && y != i && i > y) {
-                    this.el.setStyle(col, "background-color", "transparent");
+                    this.el.setStyle(col, "background-color", "");
                     this.el.setStyle(col, "background-color", "yellow");
                     this.el.setComments(col, i18n.t('static.message.planningUnitAlreadyExists'));
                     i = -1;
@@ -406,13 +406,13 @@ export default class PlanningUnitSetting extends Component {
                 var map = new Map(Object.entries(json[i]));
                 var planningUnitValue = map.get("1");
                 if (planningUnitValue == value && y != i) {
-                    this.el.setStyle(col, "background-color", "transparent");
+                    this.el.setStyle(col, "background-color", "");
                     this.el.setStyle(col, "background-color", "yellow");
                     this.el.setComments(col, i18n.t('static.message.planningUnitAlreadyExists'));
                     // this.el.setValueFromCoords(11, y, 1, true);
                     i = -1;
                 } else {
-                    this.el.setStyle(col, "background-color", "transparent");
+                    this.el.setStyle(col, "background-color", "");
                     this.el.setComments(col, "");
                     // this.el.setValueFromCoords(11, y, 1, true);
                 }
